ca56f2bbe75b84c8fba8a163cb960b957bf53b4a
Amr Mohamed <amrmuhmedd@gmail.com>
2023-07-02 18:02:31 +0300

    fix fonts issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,10 @@
 import "@/styles/globals.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/space-mono/400.css";
+import "@fontsource/space-mono/700.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
