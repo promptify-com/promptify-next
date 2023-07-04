2661de12cb9e2aa14bf405279cb5b4acb5ccaf08
Amr Mohamed <amrmuhmedd@gmail.com>
2023-07-05 00:02:42 +0300

    add linkedin  page

---
+++ src/pages/linkedin/inedex.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LinkedInCallback } from "react-linkedin-login-oauth2";
+
+function Linkedin() {
+  return (
+    <>
+      <LinkedInCallback />
+    </>
+  );
+}
+
+export default Linkedin;
