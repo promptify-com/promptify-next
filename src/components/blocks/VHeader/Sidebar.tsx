067202f6e50d5a9ab06885cd40f45da9d7567336
SALMAN <elfadili.salman@gmail.com>
2023-07-07 00:05:45 +0100

    sidebar help nav link

--- src/components/blocks/VHeader/Sidebar.tsx
+++ src/components/blocks/VHeader/Sidebar.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
 import {
   Box,
   Drawer,
+  Icon,
   ListItem,
   ListItemButton,
   Stack,
@@ -10,13 +10,14 @@ import {
 } from "@mui/material";
 import { LogoApp } from "@/assets/icons/LogoApp";
 import Link from "next/link";
-import { AutoStoriesOutlined, BookmarkAddOutlined, HomeOutlined, Search } from "@mui/icons-material";
+import { AutoStoriesOutlined, BookmarkAddOutlined, HomeOutlined, Search, Support } from "@mui/icons-material";
 
 const navItems = [
   { name: "Home", href: "/", icon: <HomeOutlined /> },
   { name: "Explore", href: "/", icon: <Search /> },
   { name: "Learn", href: "/", icon: <AutoStoriesOutlined /> },
-  { name: "Favorites", href: "/", icon: <BookmarkAddOutlined /> }
+  { name: "Favorites", href: "/", icon: <BookmarkAddOutlined /> },
+  { name: "Help", href: "/", icon: <Support /> },
 ];
 
 interface Props {
@@ -44,18 +45,23 @@ export const Sidebar: React.FC<Props> = () => {
       variant="permanent"
       anchor="left"
     >
-      <Stack gap={1}>
+      <Stack height={'100%'} gap={1}>
         <Box sx={{ p: '24px', textAlign: 'center' }}>
           <Link href="/">
-            <LogoApp width={40} />
+            <LogoApp width={30} />
           </Link>
         </Box>
         {navItems.map((item, i) => (
-          <ListItem key={i} disablePadding>
+          <ListItem key={i}
+            sx={{ 
+              p: 0, 
+              mt: (i === navItems.length - 1) ? 'auto' : 0 
+            }}
+          >
             <ListItemButton sx={{ p: '8px' }}>
               <Link href={item.href} style={{ textDecoration: "none", width: '100%' }}>
                 <Stack sx={{ flex: 1, alignItems: 'center', textAlign: 'center', color: 'onSurface' }}>
-                  <Box sx={{ p: '12px' }}>{item.icon}</Box>
+                  <Icon sx={{ p: '12px' }}>{item.icon}</Icon>
                   <Typography fontSize={12} fontWeight={400} color={'onSurface'}>{item.name}</Typography>
                 </Stack>
               </Link>
