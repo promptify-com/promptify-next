66d630df24de0709c3475483b5afe0578a844d7f
nannuflay <mohameddcharafi@gmail.com>
2023-07-14 19:49:12 +0100

    add required icons

---
+++ src/assets/icons/FavoriteList.tsx
@@ -0,0 +1,18 @@
+export const FavoriteList: React.FC = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.8">
+        <path
+          d="M21 23.09L23.45 24.58L22.8 21.77L25 19.89L22.11 19.64L21 17L19.87 19.64L17 19.89L19.18 21.77L18.5 24.58L21 23.09ZM8 18H16V20H8V18ZM8 10H20V12H8V10ZM8 14H20V16H8V14Z"
+          fill="#1B1B1E"
+        />
+      </g>
+    </svg>
+  );
+};
