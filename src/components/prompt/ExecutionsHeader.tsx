d75a44ac940924421f8e65fa15170f6c51af28d9
SALMAN <elfadili.salman@gmail.com>
2023-07-27 13:54:36 +0100

    separate into small components to use independently

--- src/components/prompt/ExecutionsHeader.tsx
+++ src/components/prompt/ExecutionsHeader.tsx
@@ -24,32 +24,71 @@ export const ExecutionsHeader: React.FC<Props> = ({
    const [searchText, setSearchText] = useState("");
    const [presetsAnchor, setPresetsAnchor] = useState<HTMLElement | null>(null);
 
-   const searchInput = (
-      <Stack direction={"row"} alignItems={"center"} spacing={1}
-         sx={{
-            position: "sticky",
-            top: 0,
-            right: 0,
-            bgcolor: "surface.2",
-            p: "5px 10px",
-            borderRadius: "99px",
-            color: "onSurface",
-         }}
-      >
-         <SubjectIcon />
-         <InputBase
-            id="exec"
-            placeholder={"Search..."}
-            fullWidth
-            sx={{
-               flex: 1,
-               fontSize: 13, 
-               fontWeight: 400
-            }}
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-         />
+   const SearchInput = (
+               
+      <Stack direction={"row"} alignItems={"center"} ml="auto">
+         <Collapse orientation="horizontal" in={searchShown}>
+            <Stack direction={"row"} alignItems={"center"} spacing={1}
+               sx={{
+                  position: "sticky",
+                  top: 0,
+                  right: 0,
+                  bgcolor: "surface.2",
+                  p: "5px 10px",
+                  borderRadius: "99px",
+                  color: "onSurface",
+               }}
+            >
+               <SubjectIcon />
+               <InputBase
+                  id="exec"
+                  placeholder={"Search..."}
+                  fullWidth
+                  sx={{
+                     flex: 1,
+                     fontSize: 13, 
+                     fontWeight: 400
+                  }}
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+               />
+            </Stack>
+         </Collapse>
+         <IconButton sx={{ ...iconButtonStyle }}
+            onClick={() => setSearchShown(!searchShown)}
+         >
+            {searchShown ? <Close /> : <SearchIcon />}
+         </IconButton>
       </Stack>
+   )
+
+   const SparksSelect = (
+      <Button
+         sx={{ 
+            width: "360px",
+            color: "onSurface", 
+            fontSize: 13, 
+            fontWeight: 500,
+            justifyContent: "space-between",
+            ":hover": { bgcolor: "action.hover" }
+         }}
+         startIcon={<FeedOutlined />}
+         endIcon={Boolean(presetsAnchor) ? <ArrowDropUp /> : <ArrowDropDown />}
+         variant={"text"}
+         onClick={(e) => setPresetsAnchor(e.currentTarget)}
+      >
+         <Box sx={{ width: "80%", overflow: "hidden", textAlign: "left" }}>
+            {selectedExecution?.title || "Choose execution"}
+         </Box>
+      </Button>
+   )
+
+   const FavoriteButton = (
+      <IconButton sx={{ ...iconButtonStyle, opacity: .5 }}
+         onClick={pinExecution}
+      >
+         {selectedExecution?.is_favorite ? <PushPin /> : <PushPinOutlined />}
+      </IconButton>
    )
 
    return (
@@ -61,77 +100,19 @@ export const ExecutionsHeader: React.FC<Props> = ({
          }}
       >
          <Box sx={{ position: "relative" }}>
+
+            {/* Big screen header */}
             <Stack
+               display={{ xs: "none", md: "flex" }}
                direction={"row"}
                alignItems={"center"}
                gap={1}
             >
-               <IconButton sx={{ ...iconButtonStyle, opacity: .5 }}
-                  onClick={pinExecution}
-               >
-                  {selectedExecution?.is_favorite ? <PushPin /> : <PushPinOutlined />}
-               </IconButton>
-               <Button
-                  sx={{ 
-                     width: "360px",
-                     color: "onSurface", 
-                     fontSize: 13, 
-                     fontWeight: 500,
-                     justifyContent: "space-between",
-                     ":hover": { bgcolor: "action.hover" }
-                  }}
-                  startIcon={<FeedOutlined />}
-                  endIcon={Boolean(presetsAnchor) ? <ArrowDropUp /> : <ArrowDropDown />}
-                  variant={"text"}
-                  onClick={(e) => setPresetsAnchor(e.currentTarget)}
-               >
-                  <Box sx={{ width: "80%", overflow: "hidden", textAlign: "left" }}>
-                     {selectedExecution?.title || "Choose execution"}
-                  </Box>
-               </Button>
-               <Popper
-                  open={Boolean(presetsAnchor)}
-                  anchorEl={presetsAnchor}
-                  transition
-                  disablePortal
-               >
-                  {({ TransitionProps, placement }) => (
-                     <Grow {...TransitionProps} style={{ transformOrigin: "center top" }}>
-                        <Paper
-                           sx={{
-                              bgcolor: "surface.1",
-                              borderRadius: "16px",
-                              overflow: "hidden",
-                              boxShadow: "0px 10px 13px -6px rgba(0, 0, 0, 0.20), 0px 20px 31px 3px rgba(0, 0, 0, 0.14), 0px 8px 38px 7px rgba(0, 0, 0, 0.12)"
-                           }}
-                           elevation={0}
-                        >
-                           <ClickAwayListener onClickAway={() => setPresetsAnchor(null)}>
-                              <Box>
-                                 <ExecutionsTabs 
-                                    executions={executions}
-                                    chooseExecution={(exec) => {
-                                       changeSelectedExecution(exec)
-                                       setPresetsAnchor(null)
-                                    }}
-                                 />
-                              </Box>
-                           </ClickAwayListener>
-                        </Paper>
-                     </Grow>
-                  )}
-               </Popper>
+               {FavoriteButton}
 
-               <Stack direction={"row"} alignItems={"center"} ml="auto">
-                  <Collapse orientation="horizontal" in={searchShown}>
-                     {searchInput}
-                  </Collapse>
-                  <IconButton sx={{ ...iconButtonStyle }}
-                     onClick={() => setSearchShown(!searchShown)}
-                  >
-                     {searchShown ? <Close /> : <SearchIcon />}
-                  </IconButton>
-               </Stack>
+               {SparksSelect}
+
+               {SearchInput}
 
                {false && (
                   <React.Fragment>
@@ -156,6 +137,49 @@ export const ExecutionsHeader: React.FC<Props> = ({
                   </React.Fragment>
                )}
             </Stack>
+
+            {/* Small screen header */}
+            <Stack
+               display={{ md: "none" }}
+               direction={"row"}
+               alignItems={"center"}
+               gap={1}
+            >
+               {SparksSelect}
+            </Stack>
+
+            <Popper
+               open={Boolean(presetsAnchor)}
+               anchorEl={presetsAnchor}
+               transition
+               disablePortal
+            >
+               {({ TransitionProps, placement }) => (
+                  <Grow {...TransitionProps} style={{ transformOrigin: "center top" }}>
+                     <Paper
+                        sx={{
+                           bgcolor: "surface.1",
+                           borderRadius: "16px",
+                           overflow: "hidden",
+                           boxShadow: "0px 10px 13px -6px rgba(0, 0, 0, 0.20), 0px 20px 31px 3px rgba(0, 0, 0, 0.14), 0px 8px 38px 7px rgba(0, 0, 0, 0.12)"
+                        }}
+                        elevation={0}
+                     >
+                        <ClickAwayListener onClickAway={() => setPresetsAnchor(null)}>
+                           <Box>
+                              <ExecutionsTabs 
+                                 executions={executions}
+                                 chooseExecution={(exec) => {
+                                    changeSelectedExecution(exec)
+                                    setPresetsAnchor(null)
+                                 }}
+                              />
+                           </Box>
+                        </ClickAwayListener>
+                     </Paper>
+                  </Grow>
+               )}
+            </Popper>
          </Box>
       </Box>
    )
