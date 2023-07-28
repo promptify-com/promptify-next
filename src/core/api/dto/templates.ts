7eef25e720ae695f22a05df5ed5cef65cd9413af
SALMAN <elfadili.salman@gmail.com>
2023-07-28 11:18:39 +0100

    Spark types

--- src/core/api/dto/templates.ts
+++ src/core/api/dto/templates.ts
@@ -120,28 +120,16 @@ export interface ITemplateExecutionPut {
 }
 
 export interface PromptExecutions {
-  content: string;
-  created_at: Date;
-  executed_by: number;
   id: number;
+  prompt: number;
+  executed_by: number;
   output: string;
   parameters: {
     [key: number]: string | number;
   };
-  prompt: number;
-}
-export interface PromptExecutionResponse {
-  contextual_overrides: {
-    [key: number]: [];
-  };
+  content: string;
   created_at: Date;
-  executed_by: number;
-  id: number;
-  parameters: {
-    [key: number]: string | number;
-  };
-  prompt_executions: PromptExecutions[];
-  template: number;
+  tokens_spent: number;
 }
 
 export interface TemplateExecutionsDisplay {
@@ -162,4 +150,34 @@ export interface TemplateExecutionsDisplay {
     created_at: string;
   }[];
   likes: number;
+}
+export interface SparkExecution {
+  id: number;
+  title: string;
+  parameters: {
+    [key: number]: string | number;
+  };
+  contextual_overrides: {
+    [key: number]: [];
+  };
+  template: number;
+  executed_by: number;
+  created_at: string;
+  prompt_executions: PromptExecutions[];
+  is_favorite: boolean;
+}
+
+export interface SparkVersion {
+  id: number,
+  title: string,
+  created_at: string
+}
+export interface Spark {
+  id: number,
+  initial_title: string,
+  created_at: string,
+  created_by: number,
+  versions: SparkVersion[],
+  current_version: TemplatesExecutions,
+  is_favorite: false
 }
