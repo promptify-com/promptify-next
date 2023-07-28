9b3157d50cd1a6db25742529c7f627136c420d2c
Dmytro Chornovol <dbtheoldb@gmail.com>
2023-07-28 14:21:08 +0300

    added floating image to generated execution card

--- src/components/prompt/ExecutionCardGenerated.tsx
+++ src/components/prompt/ExecutionCardGenerated.tsx
@@ -29,6 +29,9 @@ export const ExecutionCardGenerated: React.FC<Props> = ({ execution, templateDat
       }}
     >
       {execution.data?.map((exec, i) => {
+        const prevItem = i > 0 && execution.data && execution.data[i - 1];
+        const isPrevItemIsImage = prevItem && isImageOutput(prevItem?.message);
+
         const prompt = templateData.prompts.find(
           (prompt) => prompt.id === exec.prompt
         );
@@ -41,36 +44,44 @@ export const ExecutionCardGenerated: React.FC<Props> = ({ execution, templateDat
               <Subtitle sx={{ fontSize: 24, fontWeight: 400, color: "onSurface" }}>
                 {prompt.title}
               </Subtitle>
-              {isImageOutput(exec.message) ? (
-                <Box
-                  component={"img"}
-                  alt={"book cover"}
-                  src={exec.message}
-                  onError={(
-                    e: React.SyntheticEvent<HTMLImageElement, Event>
-                  ) => {
-                    (e.target as HTMLImageElement).src =
-                      "http://placehold.it/165x215";
-                  }}
-                  sx={{
-                    borderRadius: "8px",
-                    width: 165,
-                    height: 215,
-                    objectFit: "cover",
-                  }}
-                />
-              ) : (
-                <Typography
-                  sx={{
-                    fontSize: 15,
-                    fontWeight: 400,
-                    color: "onSurface",
-                    wordWrap: "break-word",
-                  }}
-                  dangerouslySetInnerHTML={{
-                    __html: getMarkdownFromString(exec.message),
-                  }}
-                />
+              {!isImageOutput(exec.message) && (
+                <Box>
+                  {isPrevItemIsImage && (
+                    <Box
+                      component={"img"}
+                      alt={"book cover"}
+                      src={prevItem.message}
+                      onError={(
+                        e: React.SyntheticEvent<HTMLImageElement, Event>
+                      ) => {
+                        (e.target as HTMLImageElement).src =
+                          "http://placehold.it/165x215";
+                      }}
+                      sx={{
+                        borderRadius: "8px",
+                        width: 165,
+                        height: 215,
+                        objectFit: "cover",
+                        float: "right",
+                        ml: "10px",
+                        mb: "10px",
+                      }}
+                    />
+                  )}
+                  <Typography
+                    sx={{
+                      fontSize: 15,
+                      fontWeight: 400,
+                      color: "onSurface",
+                      wordWrap: "break-word",
+                      textAlign: "justify",
+                      float: "none",
+                    }}
+                    dangerouslySetInnerHTML={{
+                      __html: getMarkdownFromString(exec.message),
+                    }}
+                  />
+                </Box>
               )}
             </Stack>
           );
