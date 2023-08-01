efd6b4f615eff930dda386567b414dffa9240d7a
SALMAN <elfadili.salman@gmail.com>
2023-08-01 13:26:41 +0100

    image upload preview

--- src/components/common/forms/TemplateForm.tsx
+++ src/components/common/forms/TemplateForm.tsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { object, string } from 'yup';
 import { useFormik } from 'formik';
-import { Select, MenuItem, TextField, Input, Chip, Autocomplete, InputLabel, Checkbox, Stack } from '@mui/material';
+import { Select, MenuItem, TextField, Chip, Autocomplete, InputLabel, Checkbox, Stack } from '@mui/material';
 import {  useGetTagsQuery } from '@/core/api/explorer';
 import { Templates } from '@/core/api/dto/templates';
 import { IEditTemplate } from '@/common/types/editTemplate';
@@ -12,7 +12,6 @@ import { createTemplate, updateTemplate } from '@/hooks/api/templates';
 import { authClient } from '@/common/axios';
 import {fieldStyle, boxStyle, buttonBoxStyle, typographyStyle, checkboxStyle} from '../../modals/styles'
 import { useGetCategoriesQuery } from '@/core/api/categories';
-import BaseButton from '@/components/base/BaseButton';
 import { Upload } from '@mui/icons-material';
 
 interface Props {
@@ -51,11 +50,15 @@ const TemplateForm: React.FC<Props> = ({
     );
   }, [selectedTags]);
 
+  // TODO - No need to upload image and then preview it, just preview it and upload on submit
   const getUrlImage = async (image: File) => {
     const file = new FormData();
     file.append('file', image);
     const { data: { file_url } } = await authClient.post('/api/upload/', file);
-    formik.values.thumbnail = file_url;
+    formik.setFieldValue(
+      'thumbnail', 
+      file_url
+    );
   };
 
   const addNewTag = (newValue: string[]) => {
@@ -139,40 +142,45 @@ const TemplateForm: React.FC<Props> = ({
         <Typography sx={typographyStyle}>Thumbnail</Typography>
         <Box display="flex" flexDirection="column" width="250px">
           {formik.values.thumbnail && (
-            <img src={formik.values.thumbnail} alt="thumbnail"  />
+            <img 
+              src={formik.values.thumbnail} 
+              alt="thumbnail" 
+              style={{ height: "250px", objectFit: "cover" }}
+            />
           )}
-          {/* <Input
-            type="file"
-            sx={{ width: '250px' }}
-            onChange={(args: any) => {
-              getUrlImage(args.target.files[0]);
-            }}
-          /> */}
-          <BaseButton
-            variant="contained"
-            icon={<Upload sx={{ height: "100%", display: "flex", alignItems: "center", fontSize: "1.2rem", mr: "15px" }} />}
-            color="custom"
-            customColor="#82899e"
-            sx={{ borderRadius: "4px" }}
-          >
-            <Typography sx={{
-                fontSize: "1rem",
-                fontWeight: 400,
-                color: "inherit",
+          <Box component="label">
+            <Stack direction={"row"} alignItems={"center"} justifyContent={"center"}
+              sx={{ 
+                bgcolor: "grey.600",
+                color: "common.white",
+                border: "1px solid transparent", 
+                borderRadius: "4px",
+                p: "8px",
+                mt: "5px",
+                cursor: "pointer",
+                ":hover": { bgcolor: "transparent", color: "grey.600", borderColor: "grey.600" } 
               }}
             >
-              Select Image
-            </Typography>
+              <Upload sx={{ height: "100%", display: "flex", alignItems: "center", fontSize: "1.2rem", mr: "15px" }} />
+              <Typography sx={{
+                  fontSize: "1rem",
+                  fontWeight: 400,
+                  color: "inherit",
+                }}
+              >
+                Select Image
+              </Typography>
 
-            <input
-              hidden
-              accept="image/*"
-              type="file"
-              onChange={(args: any) => {
-                getUrlImage(args.target.files[0]);
-              }}
-            />
-          </BaseButton>
+              <input
+                hidden
+                accept="image/*"
+                type="file"
+                onChange={(args: any) => {
+                  getUrlImage(args.target.files[0]);
+                }}
+              />
+            </Stack>
+          </Box>
         </Box>
       </Box>
 
