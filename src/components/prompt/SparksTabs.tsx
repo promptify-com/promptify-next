9a1789de44d563046ca1f7a57518f10ebaf09786
SALMAN <elfadili.salman@gmail.com>
2023-08-04 10:09:52 +0100

    display spark created_at if its empty - no current_version

--- src/components/prompt/SparksTabs.tsx
+++ src/components/prompt/SparksTabs.tsx
@@ -90,7 +90,10 @@ export const SparksTabs:React.FC<Props> = ({ sparks, chooseSpark }) => {
                      opacity: .5
                   }}
                   >
-                     {spark.current_version ? moment(spark.current_version.created_at).fromNow() : '-'}
+                     {spark.current_version ? 
+                        moment(spark.current_version.created_at).fromNow() : 
+                        moment(spark.created_at).fromNow()
+                     }
                   </Typography>
                </Stack>
             </Stack>
