0117a84db2e231cc526144584ce3e7d1ddbc3f6c
nannuflay <mohameddcharafi@gmail.com>
2023-08-08 19:56:07 +0100

    feat: add update title endpoint

--- src/core/api/sparks.ts
+++ src/core/api/sparks.ts
@@ -1,4 +1,8 @@
-import { Spark, TemplateExecutionsDisplay } from "./dto/templates";
+import {
+  ISparkWithTemplate,
+  Spark,
+  TemplateExecutionsDisplay,
+} from "./dto/templates";
 import useDeferredAction from "../../hooks/useDeferredAction";
 import { authClient } from "../../common/axios";
 import { baseApi } from "./api";
@@ -17,12 +21,35 @@ export const sparksApi = baseApi.injectEndpoints({
           url: `/api/meta/sparks/me`,
           method: "get",
         }),
+        providesTags: ["Sparks"],
+      }),
+
+      editSparkTitle: builder.mutation<
+        Spark,
+        { id: number; data: ISparkWithTemplate }
+      >({
+        query: ({
+          id,
+          data,
+        }: {
+          id: number | undefined;
+          data: ISparkWithTemplate;
+        }) => ({
+          url: `/api/meta/sparks/${id}/`,
+          method: "patch",
+          data: data,
+        }),
+        invalidatesTags: ["Sparks"],
       }),
     };
   },
 });
 
-export const { useGetSparksByTemplateQuery, useGetSparksByMeQuery } = sparksApi;
+export const {
+  useGetSparksByTemplateQuery,
+  useGetSparksByMeQuery,
+  useEditSparkTitleMutation,
+} = sparksApi;
 
 export const useTemplateView = () => {
   return useDeferredAction(async (id: number) => {
