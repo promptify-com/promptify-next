024e3277a7bd236e3c3138e2aaa84aa4b8a2ef32
Dmytro Chornovol <dbtheoldb@gmail.com>
2023-08-11 19:57:29 +0300

    changed opacity to bottom tab bg

--- src/components/prompt/BottomTabs.tsx
+++ src/components/prompt/BottomTabs.tsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Grid, IconButton, Theme, Typography, alpha, useTheme } from "@mui/material";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import {
+  Grid,
+  IconButton,
+  Theme,
+  Typography,
+  alpha,
+  useTheme,
+} from "@mui/material";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 interface Props {
   onChange: (tab: number) => void;
@@ -32,32 +39,40 @@ export const BottomTabs: React.FC<Props> = ({
         bottom: 10,
         left: 0,
         right: 0,
-        marginLeft: 'auto',
-        marginRight: 'auto',
+        marginLeft: "auto",
+        marginRight: "auto",
         zIndex: 999,
-        bgcolor: alpha(theme.palette.primary.main, 0.7),
-        width: '90%',
-        borderRadius: '50px'
+        bgcolor: alpha(theme.palette.primary.main, 0.9),
+        width: "90%",
+        borderRadius: "50px",
       }}
     >
       {mobileTabs.map((tab, i) => (
-        <Grid item xs={4} md={4} key={i} py='10px' display='flex' justifyContent='center' >
+        <Grid
+          item
+          xs={4}
+          md={4}
+          key={i}
+          py="10px"
+          display="flex"
+          justifyContent="center"
+        >
           <IconButton
             onClick={() => {
               onChange(i);
               setActiveTab(i);
             }}
             sx={{
-              display: 'flex', 
-              justifyContent: 'center',
-              width: '80%',
-              height: '42px',
-              border: 'none',
-              borderRadius: 50,          
+              display: "flex",
+              justifyContent: "center",
+              width: "80%",
+              height: "42px",
+              border: "none",
+              borderRadius: 50,
               fontSize: 16,
               fontWeight: 500,
-              bgcolor: activeTab === i ? '#FFF' : 'transparent',
-              color: activeTab === i ? '#000' : '#FFF',
+              bgcolor: activeTab === i ? "#FFF" : "transparent",
+              color: activeTab === i ? "#000" : "#FFF",
               svg: {
                 width: 20,
                 height: 20,
@@ -65,7 +80,10 @@ export const BottomTabs: React.FC<Props> = ({
             }}
           >
             {tab.icon}
-            <Typography ml='10px' sx={{ color: activeTab === i ? "#000" : "#FFF" }}>
+            <Typography
+              ml="10px"
+              sx={{ color: activeTab === i ? "#000" : "#FFF" }}
+            >
               {tab.name}
             </Typography>
           </IconButton>
