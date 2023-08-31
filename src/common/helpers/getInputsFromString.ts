1f372ccf6d89b168c8bc083203fc46def08f082a
SALMAN <elfadili.salman@gmail.com>
2023-08-31 17:45:11 +0100

    template choices field

--- src/common/helpers/getInputsFromString.ts
+++ src/common/helpers/getInputsFromString.ts
@@ -6,6 +6,8 @@ const getType = (str: string) => {
       return "number";
     case "code":
       return "code";
+    case "choices":
+      return "choices";
     default:
       return "text";
   }
@@ -18,6 +20,11 @@ export const getInputsFromString = (str: string): IPromptInput[] => {
 
   while ((match = regex.exec(str)) !== null) {
     const parts = match[1].split(":");
+    const type = getType(parts[1]);
+    const choices =
+      type === "choices" && parts[3].startsWith('"') && parts[3].endsWith('"') // options format: "option1,option2"
+        ? Array.from(new Set(parts[3].slice(1, -1).split(","))) // duplicates removed
+        : null;
 
     const obj = {
       name: parts[0],
@@ -25,11 +32,14 @@ export const getInputsFromString = (str: string): IPromptInput[] => {
         .replace(/([a-z])([A-Z])/g, "$1 $2")
         .toLowerCase()
         .replace(/^./, parts[0][0].toUpperCase()),
-      type: getType(parts[1]),
+      type: type,
       required: parts[2] ? parts[2].toLowerCase() !== "false" : true, // required by default
+      choices: choices,
     };
 
-    matches.push(obj);
+    if (!(type === "choices" && !obj.choices?.length)) {
+      matches.push(obj);
+    }
   }
 
   return matches;
