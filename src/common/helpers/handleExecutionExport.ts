64fa74f9c998e5258b7c9613f83d516c683499e2
yassine belkaid <yassine.belkaid87@gmail.com>
2023-08-31 13:13:35 +0100

    Update src/common/helpers/handleExecutionExport.ts

--- src/common/helpers/handleExecutionExport.ts
+++ src/common/helpers/handleExecutionExport.ts
@@ -1,7 +1,7 @@
 export const handleExport = async (data: any, fileType: "word" | "pdf", title: string) => {
   const isPdf = fileType === "pdf";
   try {
-    const blob = new Blob([fileData], { type: isPdf ? "application/pdf" : "application/msword" });
+    const blob = new Blob([data], { type: isPdf ? "application/pdf" : "application/msword" });
     const url = URL.createObjectURL(blob);
 
     const a = document.createElement("a");
