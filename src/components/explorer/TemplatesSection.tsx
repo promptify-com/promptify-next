2ea7135c9dd8a2828029e63173dfe2d23b7c74fe
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-09-04 10:09:53 +0100

    fix: add no template available message to explore page

--- src/components/explorer/TemplatesSection.tsx
+++ src/components/explorer/TemplatesSection.tsx
@@ -19,6 +19,7 @@ interface TemplatesSectionProps {
   hasPrev?: boolean;
   onNextPage?: () => void;
   onPrevPage?: () => void;
+  type?: string;
 }
 
 export const TemplatesSection: React.FC<TemplatesSectionProps> = ({
@@ -31,89 +32,102 @@ export const TemplatesSection: React.FC<TemplatesSectionProps> = ({
   hasPrev = false,
   onNextPage = () => {},
   onPrevPage = () => {},
+  type,
 }) => {
   return (
-    <>
-      <Box width={"100%"}>
-        {!filtred && (isLoading || !!templates?.length) && <Typography fontSize={19}>{title}</Typography>}
+    <Box width={"100%"}>
+      {!filtred && (isLoading || !!templates?.length) && <Typography fontSize={19}>{title}</Typography>}
 
-        {isLoading ? (
-          isLatestTemplates ? (
-            <Grid
-              display={"flex"}
-              flexDirection={"row"}
-              gap={"16px"}
-              alignItems={"flex-start"}
-              alignContent={"flex-start"}
-              alignSelf={"stretch"}
-              flexWrap={{ xs: "nowrap", md: "wrap" }}
-            >
-              <LatestTemplatePlaceholder count={4} />
-            </Grid>
-          ) : (
-            <CardTemplatePlaceholder count={4} />
-          )
-        ) : (
+      {isLoading ? (
+        isLatestTemplates ? (
           <Grid
             display={"flex"}
+            flexDirection={"row"}
             gap={"16px"}
-            flexDirection={isLatestTemplates ? "row" : "column"}
+            alignItems={"flex-start"}
+            alignContent={"flex-start"}
+            alignSelf={"stretch"}
             flexWrap={{ xs: "nowrap", md: "wrap" }}
-            sx={{
-              mt: "10px",
-              gap: "1em",
-              width: "100%",
-              overflow: { xs: "auto", md: "initial" },
-              WebkitOverflowScrolling: { xs: "touch", md: "initial" },
-            }}
           >
-            {isLatestTemplates ? (
-              <Grid
-                display={"flex"}
-                flexWrap={{ xs: "nowrap", md: "wrap" }}
-                sx={{
-                  gap: "1em",
-                  width: "100%",
-                  overflow: { xs: "auto", md: "initial" },
-                  WebkitOverflowScrolling: { xs: "touch", md: "initial" },
-                }}
+            <LatestTemplatePlaceholder count={4} />
+          </Grid>
+        ) : (
+          <CardTemplatePlaceholder count={4} />
+        )
+      ) : (
+        <Grid
+          display={"flex"}
+          gap={"16px"}
+          flexDirection={isLatestTemplates ? "row" : "column"}
+          flexWrap={{ xs: "nowrap", md: "wrap" }}
+          sx={{
+            mt: "10px",
+            gap: "1em",
+            width: "100%",
+            overflow: { xs: "auto", md: "initial" },
+            WebkitOverflowScrolling: { xs: "touch", md: "initial" },
+          }}
+        >
+          {isLatestTemplates ? (
+            <Grid
+              display={"flex"}
+              flexWrap={{ xs: "nowrap", md: "wrap" }}
+              sx={{
+                gap: "1em",
+                width: "100%",
+                overflow: { xs: "auto", md: "initial" },
+                WebkitOverflowScrolling: { xs: "touch", md: "initial" },
+              }}
+            >
+              {!isLoading &&
+                !!templates &&
+                templates.length > 0 &&
+                templates.map((template: TemplateExecutionsDisplay | Templates) => (
+                  <Grid key={template.id}>
+                    <CardTemplateLast
+                      key={template.id}
+                      template={template as TemplateExecutionsDisplay}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
+          ) : (
+            <Grid>
+              <TemplatesPaginatedList
+                loading={isLoading}
+                hasNext={hasNext}
+                hasPrev={hasPrev}
+                onPrevPage={onPrevPage}
+                onNextPage={onNextPage}
               >
-                {!isLoading &&
-                  !!templates &&
-                  templates.length > 0 &&
+                {!!templates?.length &&
                   templates.map((template: TemplateExecutionsDisplay | Templates) => (
                     <Grid key={template.id}>
-                      <CardTemplateLast
+                      <CardTemplate
                         key={template.id}
-                        template={template as TemplateExecutionsDisplay}
+                        template={template as Templates}
                       />
                     </Grid>
                   ))}
-              </Grid>
-            ) : (
-              <Grid>
-                <TemplatesPaginatedList
-                  loading={isLoading}
-                  hasNext={hasNext}
-                  hasPrev={hasPrev}
-                  onPrevPage={onPrevPage}
-                  onNextPage={onNextPage}
-                >
-                  {!!templates?.length &&
-                    templates.map((template: TemplateExecutionsDisplay | Templates) => (
-                      <Grid key={template.id}>
-                        <CardTemplate
-                          key={template.id}
-                          template={template as Templates}
-                        />
-                      </Grid>
-                    ))}
-                </TemplatesPaginatedList>
-              </Grid>
-            )}
-          </Grid>
-        )}
-      </Box>
-    </>
+              </TemplatesPaginatedList>
+            </Grid>
+          )}
+
+          {type === "subCategorySlug" && !isLoading && !templates?.length && (
+            <Grid
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <NotFoundIcon />
+            </Grid>
+          )}
+        </Grid>
+      )}
+    </Box>
   );
 };
