c145e8403c184cebd7908562d338f0ad1a5d54b7
yassine belkaid <yassine.belkaid87@gmail.com>
2023-09-05 13:21:51 +0100

    chore: add eslint to the project (#313)

--- src/hooks/useUser.ts
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { UserContext } from "@/contexts";
-import { IUser } from "@/common/types";
-
-const useUser = (): IUser | null => {
-  return useContext(UserContext);
-};
-
-export default useUser;
