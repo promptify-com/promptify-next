3fbe4b01011c10e752662935ae33bd73f3449772
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-09-07 11:31:43 +0100

    chore: filter categories to display only ones with templates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -159,8 +159,8 @@ const HomePage: NextPage<HomePageProps> = ({ categories }) => {
 };
 
 export async function getServerSideProps() {
-  const responseCategories = await authClient.get("/api/meta/categories/");
-  const categories = responseCategories.data;
+  const responseCategories = await authClient.get<Category[]>("/api/meta/categories/");
+  const categories = responseCategories.data?.filter(category => category.prompt_template_count);
 
   return {
     props: {
