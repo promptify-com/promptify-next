55587d8b13f7c7aec9fd5d14b9839b919c12ab0f
yassine belkaid <yassine.belkaid87@gmail.com>
2023-09-12 11:30:26 +0100

    fix: overlay spinner issue with router replace method (#359)

--- src/hooks/useBrowser.ts
+++ src/hooks/useBrowser.ts
@@ -5,7 +5,10 @@ export default function useBrowser() {
 
   const replaceHistoryByPathname = (pathname: string, delay: number = 1000) => {
     setTimeout(() => {
-      router.replace({ pathname }, undefined, { shallow: true, scroll: false });
+      // router.replace({ pathname }, undefined, { shallow: true, scroll: false });
+      // workaround for https://github.com/vercel/next.js/issues/37362
+      // router.replace causes cancellation issue which nextjs unresolved internal bug
+      window.history.replaceState({}, "", pathname);
     }, delay);
   };
 
