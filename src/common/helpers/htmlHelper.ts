74293909f5f23a3ff41844982871416951829efb
SadHR <saad.harakat5@gmail.com>
2023-09-18 11:07:34 +0100

    Refactor markdownToHTML to handle excessive indentation and prevent <pre><code> tags.

--- src/common/helpers/htmlHelper.ts
+++ src/common/helpers/htmlHelper.ts
@@ -3,8 +3,19 @@ import { remark } from "remark";
 import html from "remark-html";
 
 export const markdownToHTML = async (markdown: string) => {
-  // TODO: investigate why 8 spaces are replace by <code> tag
-  const processedContent = await remark().use(html, { sanitize: false }).process(markdown.replace("        ", ""));
+  const lines = markdown.split("\n");
+  const processedLines = lines.map(line => {
+    // Check if the line starts with more than 4 spaces
+    if (/^ {5,}/.test(line)) {
+      return line.replace(/^ {5,}/, "");
+    } else {
+      return line;
+    }
+  });
+
+  const processedMarkdown = processedLines.join("\n");
+  const processedContent = await remark().use(html, { sanitize: false }).process(processedMarkdown);
+
   // remark set language name as <code class="language-js"></code>. extract language name and put in a seperated div
   const htmlContent = processedContent.toString().replace(/<code class="language-([^"]+)"/g, (match, language) => {
     return `<div class="language-label">${language}</div><code class="language-${language}"`;
