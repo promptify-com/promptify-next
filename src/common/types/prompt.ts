4523949272277d3c2e38a8ac48d56cc7782c2a50
SALMAN <elfadili.salman@gmail.com>
2023-09-19 16:36:04 +0100

    refactor ExecutionCard

--- src/common/types/prompt.ts
+++ src/common/types/prompt.ts
@@ -1,3 +1,5 @@
+import { Prompts } from "@/core/api/dto/prompts";
+
 export interface PromptLiveResponseData {
   message: string;
   prompt: number;
@@ -21,4 +23,14 @@ export interface IPromptInput {
   defaultValue?: string | number | null;
   choices?: string[] | null;
   prompt?: number;
+}
+
+export interface DisplayPrompt {
+  prompt: number;
+  content: string;
+  created_at: Date | string;
+  errors?: string;
+  isLoading?: boolean;
+  isCompleted?: boolean;
+  isFailed?: boolean;
 }
