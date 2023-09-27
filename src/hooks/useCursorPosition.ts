3fe0f96383994d924ffd000c0689daf265288909
nannuflay <mohameddcharafi@gmail.com>
2023-09-27 15:04:14 +0100

    fix: tinny issue

--- src/hooks/useCursorPosition.ts
+++ src/hooks/useCursorPosition.ts
@@ -13,21 +13,19 @@ export function useCursorPosition(
   const [cursorPosition, setCursorPosition] = useState<CursorPosition | null>(null);
 
   useEffect(() => {
-    function handleCursorPositionChange() {
-      if (isSuggestionListVisible && divRef.current) {
-        const selection = window.getSelection();
-        if (selection && selection.rangeCount > 0) {
-          const range = selection.getRangeAt(0);
-          const rect = range.getBoundingClientRect();
-          const divRect = divRef.current.editorContainer.getBoundingClientRect();
-          const maxX = Math.min(rect.left - divRect.left, divRect.width * 0.5);
-          const cursorCoordinates = {
-            x: maxX,
-            y: rect.top - divRect.top + rect.height + 30,
-          };
+    if (isSuggestionListVisible && divRef.current) {
+      const selection = window.getSelection();
+      if (selection && selection.rangeCount > 0) {
+        const range = selection.getRangeAt(0);
+        const rect = range.getBoundingClientRect();
+        const divRect = divRef.current.editorContainer.getBoundingClientRect();
+        const maxX = Math.min(rect.left - divRect.left, divRect.width * 0.5);
+        const cursorCoordinates = {
+          x: maxX,
+          y: rect.top - divRect.top + rect.height + 30,
+        };
 
-          setCursorPosition(cursorCoordinates);
-        }
+        setCursorPosition(cursorCoordinates);
       }
     }
   }, [isSuggestionListVisible]);
