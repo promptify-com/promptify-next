3c1e27be86cf577d36f291ae499c50bc6f9bcf5b
Mohamed Charafi <mohameddcharafi@gmail.com>
2023-09-29 18:19:36 +0100

    Builder Page: Transition from styled-components to Material-UI styling (#439)
    * feature: Transition from styled-components to Material-UI styling on builder components
    
    * remove useless file
    
    * remove unused code
    
    * style: builder css modules
    
    * undo yarn.lock changes

--- src/components/builder/CustomNode.tsx
+++ src/components/builder/CustomNode.tsx
@@ -1,66 +1,12 @@
-import { PromptIcon } from "@/assets/icons";
-import { Avatar } from "@mui/material";
 import * as React from "react";
 import { ClassicScheme, RenderEmit, Presets } from "rete-react-render-plugin";
-import styled from "styled-components";
+import { Divider, Paper } from "@mui/material";
+
+import styles from "@/styles/builder.module.css";
 
 const { RefSocket } = Presets.classic;
 
 type NodeExtraData = { width?: number; height?: number; engineIcon?: string };
-
-export const NodeStyles = styled.div<NodeExtraData & { selected: boolean; styles?: (props: any) => any }>`
-  width: 300px;
-  min-height: 112px;
-  background: #fdfbff;
-  color: #1b1b1e;
-  border: 2px solid;
-  border-color: ${props => (props.selected ? `black` : "transparent")};
-  border-radius: 16px;
-  cursor: pointer;
-  box-shadow:
-    0px 6px 6px -3px rgba(225, 226, 236, 0.2),
-    0px 10px 14px 1px rgba(225, 226, 236, 0.14),
-    0px 4px 18px 3px rgba(27, 27, 30, 0.12);
-  .header {
-    font-size: 16px;
-    font-weight: 500;
-    font-family: Poppins;
-    padding: 16px;
-    display: flex;
-    gap: 8px;
-    white-space: nowrap;
-    overflow-x: hidden;
-  }
-  .header img {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-  }
-  hr {
-    border: 0.5px solid #e1e2ec;
-    margin: 0;
-  }
-  .body {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 16px 8px;
-  }
-  .input,
-  .output {
-    display: flex;
-    gap: 8px;
-    font-size: 14px;
-  }
-  .input-socket [title="socket"],
-  .output-socket [title="socket"] {
-    height: 12px;
-    width: 12px;
-    background: #e1e2ec;
-    border: none;
-  }
-  ${props => props.styles && props.styles(props)}
-`;
 
 function sortByIndex<T extends [string, undefined | { index?: number }][]>(entries: T) {
   entries.sort((a, b) => {
@@ -83,19 +29,15 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
   const outputs = Object.entries(props.data.outputs);
   const controls = Object.entries(props.data.controls);
   const selected = props.data.selected || false;
-  const { id, label, width, height, engineIcon } = props.data;
+  const { id, label, engineIcon } = props.data;
 
   sortByIndex(inputs);
   sortByIndex(outputs);
   sortByIndex(controls);
 
   return (
-    <NodeStyles
-      selected={selected}
-      styles={props.styles}
-      data-testid="node"
-    >
-      <div className="header">
+    <div className={`${styles.nodeStyles} ${selected ? styles.selected : ""}`}>
+      <div className={styles.header}>
         <img
           src={engineIcon}
           alt={label}
@@ -108,13 +50,19 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
         />
         {label}
       </div>
-      <hr />
-      <div className="body">
+      <Divider
+        sx={{
+          "&::before, &::after": {
+            borderColor: "#e1e2ec",
+          },
+        }}
+      />
+      <div className={styles.body}>
         {inputs.map(
           ([key, input]) =>
             input && (
               <div
-                className="input"
+                className={styles.ioContainer}
                 key={key}
                 data-testid={`input-${key}`}
               >
@@ -141,7 +89,7 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
           ([key, output]) =>
             output && (
               <div
-                className="output"
+                className={`${styles.ioContainer}`}
                 key={key}
                 data-testid={`output-${key}`}
               >
@@ -163,6 +111,6 @@ export function CustomNode<Scheme extends ClassicScheme>(props: Props<Scheme>) {
             ),
         )}
       </div>
-    </NodeStyles>
+    </div>
   );
 }
