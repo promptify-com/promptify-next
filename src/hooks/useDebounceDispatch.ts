32b115aedac6f388b6e4f596efcbcf86f5725175
nannuflay <mohameddcharafi@gmail.com>
2023-10-13 16:57:30 +0100

    fix: Delayed Input Response in User Builder Prompts

---
+++ src/hooks/useDebounceDispatch.ts
@@ -0,0 +1,28 @@
+import { useRef, useEffect, useCallback } from "react";
+
+export function useDebouncedDispatch<T extends (...args: any) => any>(fn: T, delay: number) {
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  const debouncedFn = useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    },
+    [fn, delay],
+  );
+
+  return debouncedFn as T;
+}
