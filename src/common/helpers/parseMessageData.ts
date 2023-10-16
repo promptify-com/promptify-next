ebad38b62a8715bfd5021914c5e60ac1ab701a2d
nannuflay <mohameddcharafi@gmail.com>
2023-10-16 19:05:27 +0100

    handle additional usecases

--- src/common/helpers/parseMessageData.ts
+++ src/common/helpers/parseMessageData.ts
@@ -1,6 +1,18 @@
 export function parseMessageData(data: string): any {
-  // Convert single quotes to double quotes
-  data = data.replace(/(\W)'|'(\W)/g, '$1"$2');
+  // Capture all 'word-ending contractions' to revert them later
+  const contractions = [];
+  const contractionRegex = /(\w)"/g;
+  let match;
+  while ((match = contractionRegex.exec(data)) !== null) {
+    contractions.push(match[1] + '"');
+  }
+
+  // Convert single quotes to double quotes only when they are outside of double quotes
+  const outsideOfDoubleQuotes = /'(?=(?:[^"]*"[^"]*")*[^"]*$)/g;
+  data = data.replace(outsideOfDoubleQuotes, '"');
+
+  // Handle double quotes that precede newlines
+  data = data.replace(/\\"\\n/g, '\\\\"\\n');
 
   // Handle cases like ""t", ""s", ""re", etc.
   data = data.replace(/""([a-z]{1,2})"/gi, '"\\"$1"');
@@ -11,6 +23,14 @@ export function parseMessageData(data: string): any {
   // Handle triple double quotes
   data = data.replace(/"""/g, '"\\""');
 
+  // Correctly escape double quotes inside message property value
+  data = data.replace(/"message": "(.*?)",/g, (match, p1) => {
+    // Ensure we're not over-escaping already correctly formatted strings
+    return `"message": "${p1.split('\\"').join('"').replace(/"/g, '\\"')}",`;
+  });
+
+  // Revert 'word-ending contractions' to their original form
+  data = data.replace(/"\\"([a-z]{1,2})"/gi, '"\'$1"');
   let parsedData;
   try {
     parsedData = JSON.parse(data);
