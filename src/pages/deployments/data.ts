34d4e58234eaf8100d76e957118c9ec7f1189d5b
nannuflay <mohameddcharafi@gmail.com>
2023-10-30 11:42:18 +0100

    remove useless data

--- src/pages/deployments/data.ts
+++ src/pages/deployments/data.ts
@@ -192,36 +192,3 @@ export const models: Model[] = [
     },
   },
 ];
-
-export const deployments: Deployment[] = [
-  {
-    model: "lorem ipsum open ai",
-    instance: "lorem ipsum open instance",
-    status: "DEPLOYING",
-    createdAt: "2 days",
-  },
-  {
-    model: "lorem ipsum open ai",
-    instance: "lorem ipsum open instance",
-    status: "DONE",
-    createdAt: "2 days",
-  },
-  {
-    model: "lorem ipsum open ai",
-    instance: "lorem ipsum open instance",
-    status: "FAILED",
-    createdAt: "2 days",
-  },
-  {
-    model: "lorem ipsum open ai",
-    instance: "lorem ipsum open instance",
-    status: "STOPPED",
-    createdAt: "2 days",
-  },
-  {
-    model: "lorem ipsum open ai",
-    instance: "lorem ipsum open instance",
-    status: "CREATED",
-    createdAt: "2 days",
-  },
-];
