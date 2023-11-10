2621cfff8e1e6f23033a642ba131635b7472750e
nannuflay <mohameddcharafi@gmail.com>
2023-11-10 15:04:15 +0100

    show static contetn when loading, fix placeholdder on mobile layout

--- src/components/placeholders/DeploymentPlaceholder.tsx
+++ src/components/placeholders/DeploymentPlaceholder.tsx
@@ -55,7 +55,7 @@ export default function DeploymentPlaceholder({ count = 6 }) {
               </Box>
             </Box>
             <Box
-              display={"flex"}
+              display={{ xs: "none", md: "flex" }}
               justifyContent={"end"}
               gap={1}
             >
@@ -69,17 +69,23 @@ export default function DeploymentPlaceholder({ count = 6 }) {
               ))}
             </Box>
           </Grid>
+        </Grid>
+        <Grid
+          gap={0.5}
+          sx={{ width: "100%" }}
+          display={{ xs: "flex", md: "none" }}
+          flexDirection={"column"}
+        >
           <Grid
-            gap={0.5}
-            sx={{ width: "100%" }}
-            display={{ xs: "flex", md: "none" }}
-            flexDirection={"column"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
           >
             <Skeleton
               variant="text"
               animation="wave"
               sx={{
-                width: "90%",
+                width: "20%",
                 height: 20,
                 marginBottom: "4px",
                 fontSize: "18px",
@@ -88,13 +94,51 @@ export default function DeploymentPlaceholder({ count = 6 }) {
             <Skeleton
               variant="text"
               animation="wave"
-              sx={{ width: "80%", height: 16 }}
+              sx={{
+                width: "20%",
+                height: 30,
+                marginBottom: "4px",
+                fontSize: "18px",
+              }}
             />
+          </Grid>
+          <Skeleton
+            variant="text"
+            animation="wave"
+            sx={{
+              width: "90%",
+              height: 20,
+            }}
+          />
+          <Grid
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+          >
             <Skeleton
               variant="text"
               animation="wave"
-              sx={{ width: "70%", height: 16 }}
+              sx={{
+                width: "20%",
+                height: 20,
+                marginBottom: "4px",
+                fontSize: "18px",
+              }}
             />
+            <Box
+              display={"flex"}
+              justifyContent={"end"}
+              gap={1}
+            >
+              {Array.from({ length: 2 }).map((_, index) => (
+                <Skeleton
+                  key={index}
+                  variant="circular"
+                  animation="wave"
+                  sx={{ width: "20px", height: "20px" }}
+                />
+              ))}
+            </Box>
           </Grid>
         </Grid>
       </Box>
--- src/pages/deployments/index.tsx
+++ src/pages/deployments/index.tsx
@@ -41,96 +41,96 @@ function Deployments() {
           padding={{ xs: "4px 0px", md: "0px 8px" }}
         >
           <Grid sx={{ padding: { xs: "16px", md: "32px" } }}>
-            {isLoading ? (
-              <Box>
+            <Stack gap={2}>
+              <Typography
+                fontSize="24px"
+                fontWeight={500}
+                color="onSurface"
+                lineHeight="34.32px"
+                letterSpacing="0.17"
+              >
+                My Deployments
+              </Typography>
+
+              <Stack
+                direction={{ xs: "column-reverse", md: "row" }}
+                gap={2}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Stack
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="start"
+                  gap={"8px"}
+                  width={"100%"}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    width="200px"
+                    borderRadius="8px"
+                  >
+                    <TextField
+                      id="filled-basic"
+                      label="Find deployment..."
+                      size="small"
+                      variant="outlined"
+                      value={searchName}
+                      onChange={e => setSearchName(e.target.value.trim().toLowerCase())}
+                    />
+                  </Box>
+                  <FormControl
+                    sx={{ minWidth: 120 }}
+                    size="small"
+                  >
+                    <InputLabel>Status</InputLabel>
+                    <Select
+                      value={status}
+                      label="Status"
+                      autoWidth
+                      onChange={e => setStatus(e.target.value as DeploymentStatus)}
+                    >
+                      {deploymentStatuses.map((status, idx) => (
+                        <MenuItem
+                          key={idx}
+                          value={status}
+                        >
+                          {status}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                </Stack>
+                <Stack
+                  direction={"row"}
+                  width={"100%"}
+                  justifyContent={"end"}
+                >
+                  <CreateDeploymentButton onRefetch={refetchData} />
+                </Stack>
+              </Stack>
+              <ActiveFilters
+                status={status}
+                onClearStatus={() => setStatus("")}
+                searchName={searchName}
+                onClearSearch={() => setSearchName("")}
+              />
+
+              {isLoading ? (
                 <Box bgcolor="surface.1">
                   <DeploymentPlaceholder count={4} />
                 </Box>
-              </Box>
-            ) : (
-              <Stack gap={2}>
-                <Typography
-                  fontSize="24px"
-                  fontWeight={500}
-                  color="onSurface"
-                  lineHeight="34.32px"
-                  letterSpacing="0.17"
-                >
-                  My Deployments
-                </Typography>
-
-                <Stack
-                  direction={{ xs: "column-reverse", md: "row" }}
-                  gap={2}
-                  justifyContent="space-between"
-                  alignItems="center"
-                >
-                  <Stack
-                    direction="row"
-                    alignItems="center"
-                    justifyContent="start"
-                    gap={"8px"}
-                    width={"100%"}
-                  >
-                    <Box
-                      display="flex"
-                      alignItems="center"
-                      width="200px"
-                      borderRadius="8px"
-                    >
-                      <TextField
-                        id="filled-basic"
-                        label="Find deployment..."
-                        size="small"
-                        variant="outlined"
-                        value={searchName}
-                        onChange={e => setSearchName(e.target.value.trim().toLowerCase())}
-                      />
-                    </Box>
-                    <FormControl
-                      sx={{ minWidth: 120 }}
-                      size="small"
-                    >
-                      <InputLabel>Status</InputLabel>
-                      <Select
-                        value={status}
-                        label="Status"
-                        autoWidth
-                        onChange={e => setStatus(e.target.value as DeploymentStatus)}
-                      >
-                        {deploymentStatuses.map((status, idx) => (
-                          <MenuItem
-                            key={idx}
-                            value={status}
-                          >
-                            {status}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </Stack>
-                  <Stack
-                    direction={"row"}
-                    width={"100%"}
-                    justifyContent={"end"}
-                  >
-                    <CreateDeploymentButton onRefetch={refetchData} />
-                  </Stack>
-                </Stack>
-                <ActiveFilters
-                  status={status}
-                  onClearStatus={() => setStatus("")}
-                  searchName={searchName}
-                  onClearSearch={() => setSearchName("")}
-                />
-
-                {filteredDeployments.length === 0 ? (
-                  <Typography sx={{ opacity: 0.4, textAlign: "center", py: 10 }}>No deployments found</Typography>
-                ) : (
-                  <DeploymentList items={filteredDeployments} />
-                )}
-              </Stack>
-            )}
+              ) : (
+                <>
+                  {filteredDeployments.length === 0 ? (
+                    <Typography sx={{ opacity: 0.4, textAlign: "center", py: 10 }}>No deployments found</Typography>
+                  ) : (
+                    <DeploymentList items={filteredDeployments} />
+                  )}
+                </>
+              )}
+            </Stack>
           </Grid>
         </Box>
       </Layout>
