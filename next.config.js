f24554548beefbefeedf311db4110574a6c8ac18
Ishtiaq Bhatti <ishtiaqbhatti455@gmail.com>
2023-11-13 12:03:59 +0500

    removed sentry to test performance

--- next.config.js
+++ next.config.js
@@ -10,27 +10,29 @@ const nextConfig = {
   },
 };
 
-if (
-  process.env.NODE_ENV === "productionasd" &&
-  process.env.NEXT_PUBLIC_SENTRY_DSN &&
-  process.env.SENTRY_AUTH_TOKEN
-) {
-  const { withSentryConfig } = require("@sentry/nextjs");
+export default nextConfig;
 
-  nextConfig.sentry = {
-    hideSourceMaps: true,
-    widenClientFileUpload: true,
-    transpileClientSDK: false,
-    tunnelRoute: "/monitoring",
-    disableLogger: true
-  };
+// if (
+//   process.env.NODE_ENV === "productionasd" &&
+//   process.env.NEXT_PUBLIC_SENTRY_DSN &&
+//   process.env.SENTRY_AUTH_TOKEN
+// ) {
+//   const { withSentryConfig } = require("@sentry/nextjs");
 
-  module.exports = withSentryConfig(nextConfig, {
-    org: "matious-corp",
-    project: "promptify-web-production",
-    silent: true,
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-  });
-} else {
-  module.exports = nextConfig;
-}
+//   nextConfig.sentry = {
+//     hideSourceMaps: true,
+//     widenClientFileUpload: true,
+//     transpileClientSDK: false,
+//     tunnelRoute: "/monitoring",
+//     disableLogger: true
+//   };
+
+//   module.exports = withSentryConfig(nextConfig, {
+//     org: "matious-corp",
+//     project: "promptify-web-production",
+//     silent: true,
+//     authToken: process.env.SENTRY_AUTH_TOKEN,
+//   });
+// } else {
+//   module.exports = nextConfig;
+// }
--- sentry.server.config.js
+++
@@ -1,9 +0,0 @@
-import { init } from "@sentry/nextjs";
-
-if (process.env.NEXT_PUBLIC_SENTRY_DSN  && process.env.NODE_ENV === 'production') {
-    init({
-        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-        tracesSampleRate: 0,
-        debug: false
-    });
-}
