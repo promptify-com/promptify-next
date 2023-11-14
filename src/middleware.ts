e8d51c2149be1d98b941b0bc55c2061679585405
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-11-14 12:00:02 +0100

    chore: fix amplify mobile detection

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,12 @@
-import { NextRequest, NextResponse, userAgent } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
+import { getDeviceType } from "./common/helpers/ua-parser";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
 
 export function middleware(request: NextRequest) {
-  const { device } = userAgent(request);
-  const viewport = device.type === "mobile" ? "mobile" : "desktop";
+  const viewport = getDeviceType(request.headers.get("user-agent"));
 
   request.nextUrl.searchParams.set("viewport", viewport);
 
