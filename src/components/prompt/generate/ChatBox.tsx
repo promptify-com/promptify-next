b316459129bcf68e416c8cd9b90f0ddb05e8614a
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-11-22 11:22:14 +0100

    fix: invalidate executions once new generation is done

--- src/components/prompt/generate/ChatBox.tsx
+++ src/components/prompt/generate/ChatBox.tsx
@@ -19,7 +19,7 @@ import { TemplateQuestions, Templates, UpdatedQuestionTemplate } from "@/core/ap
 import { IPromptInput, PromptLiveResponse, AnsweredInputType } from "@/common/types/prompt";
 import { setGeneratingStatus, updateExecutionData } from "@/core/store/templatesSlice";
 import { IAnswer, IMessage } from "@/common/types/chat";
-import { useStopExecutionMutation } from "@/core/api/executions";
+import { executionsApi, useStopExecutionMutation } from "@/core/api/executions";
 import VaryModal from "./VaryModal";
 import { vary } from "@/common/helpers/varyValidator";
 import { parseMessageData } from "@/common/helpers/parseMessageData";
@@ -205,6 +205,7 @@ const ChatMode: React.FC<Props> = ({ onError, template }) => {
       if (allPromptsCompleted) {
         selectGeneratedExecution();
         dispatch(setGeneratedExecution(null));
+        dispatch(executionsApi.util.invalidateTags(["Executions"]));
       }
     }
   }, [isGenerating, generatedExecution]);
