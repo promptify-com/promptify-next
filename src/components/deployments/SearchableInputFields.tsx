b9de972d1402e4fd4c8224527e7e673c8c790e8d
yassine belkaid <yassine.belkaid87@gmail.com>
2023-11-24 18:30:17 +0100

    chore: add infinite scroll to deployment searchable inputs (#560)

--- src/components/deployments/SearchableInputFields.tsx
+++ src/components/deployments/SearchableInputFields.tsx
@@ -1,22 +1,14 @@
 import type { FormikCreateDeployment, Instance, Model } from "@/common/types/deployments";
-import Search from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
-import InputAdornment from "@mui/material/InputAdornment";
-import InputLabel from "@mui/material/InputLabel";
-import ListSubheader from "@mui/material/ListSubheader";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
-import { type Dispatch, type SetStateAction, useCallback, useRef, useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import { useState, type HTMLAttributes, forwardRef, type SyntheticEvent } from "react";
 import DataLoading from "./DataLoading";
-import { isDesktopViewPort } from "@/common/helpers";
 import type { FormikErrors } from "formik";
 import useDebounce from "@/hooks/useDebounce";
 import useDataFetching from "@/hooks/deployments/useDataFetching";
-import InstanceLabel from "./InstanceLabel";
 
 interface Props {
-  value: string;
   name: string;
   label: string;
   setFieldValue: (
@@ -25,104 +17,112 @@ interface Props {
     shouldValidate?: boolean | undefined,
   ) => Promise<void> | Promise<FormikErrors<FormikCreateDeployment>>;
   isDeploying: boolean;
-  selectMenuProps: Record<string, any>;
   regionValue: string;
 }
+type RenderRowProps = [HTMLAttributes<HTMLLIElement>, Model | Instance, number];
 
-const isModel = (model: Model | Instance): model is Model => {
-  return "name" in model;
-};
+const isModel = (model: Model | Instance): model is Model => "name" in model;
 
-export default function SearchableInputField({
-  value,
-  name,
-  label,
-  setFieldValue,
-  isDeploying,
-  selectMenuProps,
-  regionValue,
-}: Props) {
+function RenderRow({ child }: { child: RenderRowProps }) {
+  const option = child[1];
+  const value = isModel(option)
+    ? option.name
+    : `${option.instance_type} (cost ${option.cost}/h, ${option.vcpus}vcpus, ${option.num_gpus}gpus, ${option.memory}memory)`;
+
+  return <li {...child[0]}>{value}</li>;
+}
+
+export default function SearchableInputField({ name, label, setFieldValue, isDeploying, regionValue }: Props) {
   const [searchText, setSearchText] = useState("");
   const deboundedSearchText = useDebounce(searchText, 300);
   const isModelType = name === "model";
-  const { fetchMoreData, hasMoreData, isFetching, data, isRegionEmpty } = useDataFetching(
+  const { fetchNextData, hasNextData, isFetching, data, isRegionEmpty } = useDataFetching(
     regionValue,
     isModelType ? "models" : "instances",
     deboundedSearchText,
   );
-  const isMobile = !isDesktopViewPort();
-  const observer = useRef<IntersectionObserver | null>(null);
-  const lastModelRef = useCallback(
-    (node: HTMLDivElement) => {
-      if (isFetching) return;
-      if (observer.current) {
-        observer.current.disconnect();
-      }
+  const ListboxComponent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLElement>>(
+    function ListboxComponent(props, ref) {
+      const { children, ...other } = props;
 
-      const rowHeight = isMobile ? 145 : 80;
-      const margin = `${2 * rowHeight}px`;
-
-      observer.current = new IntersectionObserver(
-        entries => {
-          if (entries[0].isIntersecting) {
-            fetchMoreData();
-          }
-
-          if (!hasMoreData) {
-            observer.current?.disconnect();
-          }
-        },
-        { rootMargin: margin },
+      return (
+        <div ref={ref}>
+          <ul {...other}>
+            {(children as RenderRowProps[])?.map(child => (
+              <RenderRow
+                key={child[1].id}
+                child={child}
+              />
+            ))}
+          </ul>
+        </div>
       );
-      if (node) {
-        observer.current.observe(node);
-      }
     },
-    [isFetching, hasMoreData, fetchMoreData],
   );
 
   return (
     <FormControl fullWidth>
-      <InputLabel>{label}</InputLabel>
-      <Select
-        value={value}
-        MenuProps={selectMenuProps}
-        label={label}
-        disabled={isDeploying ? true : isModelType ? false : isRegionEmpty}
-        variant={isModelType ? "outlined" : isRegionEmpty ? "filled" : "outlined"}
-        onChange={event => {
-          setFieldValue(name, event.target.value);
+      <Autocomplete
+        sx={{
+          ".MuiAutocomplete-endAdornment .MuiIconButton-root svg": {
+            width: "24px",
+            height: "24px",
+          },
+          ".MuiAutocomplete-endAdornment .MuiIconButton-root": {
+            border: "none",
+          },
         }}
-      >
-        <ListSubheader>
+        id={`auto-complete-${name}`}
+        onInputChange={(_, value, reason) => {
+          if (reason !== "input") {
+            return;
+          }
+
+          setSearchText(value);
+        }}
+        onChange={(_, value, reason) => {
+          if (reason !== "selectOption") {
+            return;
+          }
+
+          setFieldValue(name, value!.id);
+        }}
+        getOptionLabel={(option: Model | Instance) => {
+          return isModel(option)
+            ? option.name
+            : `${option.instance_type} (cost ${option.cost}/h, ${option.vcpus}vcpus, ${option.num_gpus}gpus, ${option.memory}memory)`;
+        }}
+        ListboxComponent={ListboxComponent}
+        ListboxProps={{
+          onScroll: (event: SyntheticEvent) => {
+            const listboxNode = event.currentTarget;
+
+            if (listboxNode.scrollTop + listboxNode.clientHeight === listboxNode.scrollHeight && hasNextData) {
+              fetchNextData();
+            }
+          },
+        }}
+        noOptionsText={`No ${name}s found.`}
+        options={data ?? []}
+        loading={isFetching}
+        disabled={isDeploying ? true : isModelType ? false : isRegionEmpty}
+        renderInput={params => (
           <TextField
-            size="small"
-            autoFocus
-            placeholder="Type to search..."
-            fullWidth
+            {...params}
+            label={label}
             InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  <DataLoading loading={isFetching} />
+                  {params.InputProps.endAdornment}
+                </>
               ),
             }}
-            onChange={e => setSearchText(e.target.value)}
           />
-        </ListSubheader>
-        {data &&
-          data.map(_data => (
-            <MenuItem
-              key={_data.id}
-              value={_data.id}
-            >
-              {isModel(_data) ? _data.name : <InstanceLabel instance={_data} />}
-            </MenuItem>
-          ))}
-        {!isFetching && !data.length && <MenuItem disabled>{`No ${name}s found.`}</MenuItem>}
-        <DataLoading loading={isFetching} />
-        <div ref={lastModelRef}></div>
-      </Select>
+        )}
+        renderOption={(props, option, state) => [props, option, state.index] as React.ReactNode}
+      />
     </FormControl>
   );
 }
