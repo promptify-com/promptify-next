df0aa7893d21274ab3b0f6ce0336effe7b77a912
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-12-05 10:19:48 +0100

    chore: update run button inside input field

--- src/components/prompt/generate/MessageSender.tsx
+++ src/components/prompt/generate/MessageSender.tsx
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import PlayCircleFilledWhiteOutlined from "@mui/icons-material/PlayCircleFilledWhiteOutlined";
 import { useAppDispatch, useAppSelector } from "@/hooks/useStore";
 import { setAccordionChatMode } from "@/core/store/templatesSlice";
-import { theme } from "@/theme";
+import SlowMotionVideo from "@mui/icons-material/SlowMotionVideo";
 
 interface MessageSenderProps {
   onSubmit: (value: string) => void;
@@ -56,129 +56,55 @@ function MessageSender({
       p={"8px 16px"}
     >
       <KeyboardCommandKey
-        sx={{ fontSize: "20px", color: "text.secondary", position: "absolute", left: 9, top: 25, opacity: 0.5 }}
+        sx={{ fontSize: "20px", color: "text.secondary", position: "absolute", left: 9, top: 12, opacity: 0.5 }}
       />
 
-      {mode === "chat" ? (
-        <Box width={{ md: "90%", xs: "87%" }}>
-          <InputBase
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-            multiline
-            disabled={disabled}
-            fullWidth
-            sx={{
-              flex: 1,
-              fontSize: 13,
-              p: "3px",
-              ml: "20px",
-              mr: "40px",
-              color: "onSurface",
-              lineHeight: "22px",
-              letterSpacing: "0.46px",
-              fontWeight: "500",
-              maxHeight: "60px",
-              overflow: "auto",
-              "&::-webkit-scrollbar": {
-                width: "3px",
-                p: 1,
-                backgroundColor: "surface.5",
-              },
-              "&::-webkit-scrollbar-track": {
-                webkitBoxShadow: "inset 0 0 16px rgba(0,0,0,0.00)",
-              },
-              "&::-webkit-scrollbar-thumb": {
-                backgroundColor: "surface.3",
-                outline: "1px solid surface.1",
-                borderRadius: "10px",
-              },
-              ".Mui-disabled": {
-                cursor: disabled ? "not-allowed" : "auto",
-              },
-            }}
-            placeholder={placeholder}
-            inputProps={{ "aria-label": "Name" }}
-            onChange={e => setLocalValue(e.target.value)}
-            value={localValue}
-            onKeyPress={handleKeyPress}
-          />
-
-          <Button
-            onClick={() => {
-              dispatch(setAccordionChatMode("execution"));
-              onGenerate?.();
-            }}
-            startIcon={<PlayCircleFilledWhiteOutlined />}
-            disabled={!showGenerate || isGenerating}
-            sx={{
-              height: "22px",
-              p: { xs: "8px", md: "15px" },
-              fontSize: { xs: 12, md: 13 },
-              ml: "20px",
-              mt: "5px",
-              lineHeight: "110%",
-              letterSpacing: "0.2px",
-              fontWeight: 500,
-              color: showGenerate && !isGenerating ? "primary" : `#ACACBE!important`,
-              bgcolor: showGenerate && !isGenerating ? "#375CA9" : `#F7F7F8!important`,
-              borderColor: showGenerate && !isGenerating ? "#375CA9" : "transparent",
-              ":hover": {
-                bgcolor: "action.hover",
-              },
-            }}
-            variant={"contained"}
-          >
-            Run prompts
-          </Button>
-        </Box>
-      ) : (
-        <InputBase
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          multiline
-          disabled={disabled}
-          fullWidth
-          sx={{
-            flex: 1,
-            fontSize: 13,
-            p: "3px",
-            ml: "20px",
-            mr: "40px",
-            color: "onSurface",
-            lineHeight: "22px",
-            letterSpacing: "0.46px",
-            fontWeight: "500",
-            maxHeight: "60px",
-            overflow: "auto",
-            "&::-webkit-scrollbar": {
-              width: "3px",
-              p: 1,
-              backgroundColor: "surface.5",
-            },
-            "&::-webkit-scrollbar-track": {
-              webkitBoxShadow: "inset 0 0 16px rgba(0,0,0,0.00)",
-            },
-            "&::-webkit-scrollbar-thumb": {
-              backgroundColor: "surface.3",
-              outline: "1px solid surface.1",
-              borderRadius: "10px",
-            },
-            ".Mui-disabled": {
-              cursor: disabled ? "not-allowed" : "auto",
-            },
-          }}
-          placeholder={placeholder}
-          inputProps={{ "aria-label": "Name" }}
-          onChange={e => setLocalValue(e.target.value)}
-          value={localValue}
-          onKeyPress={handleKeyPress}
-        />
-      )}
+      <InputBase
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        multiline
+        disabled={disabled}
+        fullWidth
+        sx={{
+          flex: 1,
+          fontSize: 13,
+          p: "3px",
+          ml: "20px",
+          mr: "40px",
+          color: "onSurface",
+          lineHeight: "22px",
+          letterSpacing: "0.46px",
+          fontWeight: "500",
+          maxHeight: "60px",
+          overflow: "auto",
+          "&::-webkit-scrollbar": {
+            width: "3px",
+            p: 1,
+            backgroundColor: "surface.5",
+          },
+          "&::-webkit-scrollbar-track": {
+            webkitBoxShadow: "inset 0 0 16px rgba(0,0,0,0.00)",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            backgroundColor: "surface.3",
+            outline: "1px solid surface.1",
+            borderRadius: "10px",
+          },
+          ".Mui-disabled": {
+            cursor: disabled ? "not-allowed" : "auto",
+          },
+        }}
+        placeholder={placeholder}
+        inputProps={{ "aria-label": "Name" }}
+        onChange={e => setLocalValue(e.target.value)}
+        value={localValue}
+        onKeyPress={handleKeyPress}
+      />
 
       <Box
         sx={{
           position: "absolute",
-          top: 25,
+          top: 6,
           right: 13,
           padding: "4px",
           width: "25px",
@@ -191,12 +117,28 @@ function MessageSender({
           color: { xs: !hasNoValue ? "#8E8E94" : "white", md: "white" },
         }}
       >
-        <ArrowUpward
-          onClick={handleSubmit}
-          sx={{
-            cursor: disabled ? "not-allowed" : "pointer",
-          }}
-        />
+        {mode === "chat" && showGenerate && !localValue ? (
+          <SlowMotionVideo
+            onClick={() => {
+              if (isGenerating) {
+                return;
+              }
+              dispatch(setAccordionChatMode("execution"));
+              onGenerate?.();
+            }}
+            sx={{
+              color: isGenerating ? "text.secondary" : "#375CA9",
+              cursor: isGenerating ? "not-allowed" : "pointer",
+            }}
+          />
+        ) : (
+          <ArrowUpward
+            onClick={handleSubmit}
+            sx={{
+              cursor: disabled ? "not-allowed" : "pointer",
+            }}
+          />
+        )}
       </Box>
     </Box>
   );
