6e1b16024ed37e93d00e439e5f2e29b92032ff5c
Yassine Belkaid <yassine.belkaid87@gmail.com>
2023-12-23 12:20:49 +0100

    fix: open automation page for all users

--- src/pages/automation/index.tsx
+++ src/pages/automation/index.tsx
@@ -6,88 +6,80 @@ import { useGetWorkflowsQuery } from "@/core/api/workflows";
 import CardWorkflow from "@/components/common/cards/CardWorkflow";
 import CardTemplatePlaceholder from "@/components/placeholders/CardTemplatePlaceHolder";
 
-const Sparks = () => {
+const Automation = () => {
   const { data: workflows, isLoading } = useGetWorkflowsQuery();
 
   return (
-    <Protected>
-      <Layout>
-        <Box
-          mt={{ xs: 7, md: 0 }}
-          padding={{ xs: "4px 0px", md: "0px 8px" }}
+    <Layout>
+      <Box
+        mt={{ xs: 7, md: 0 }}
+        padding={{ xs: "4px 0px", md: "0px 8px" }}
+      >
+        <Grid
+          sx={{
+            padding: { xs: "16px", md: "32px" },
+          }}
         >
-          <Grid
-            sx={{
-              padding: { xs: "16px", md: "32px" },
-            }}
-          >
-            {isLoading ? (
-              <>
-                <Box
-                  width={{ xs: "40%", md: "20%" }}
-                  mb={1}
-                >
-                  <Skeleton
-                    variant="text"
-                    height={35}
-                    width={"100%"}
-                    animation="wave"
-                  />
-                </Box>
+          {isLoading ? (
+            <>
+              <Box
+                width={{ xs: "40%", md: "20%" }}
+                mb={1}
+              >
+                <Skeleton
+                  variant="text"
+                  height={35}
+                  width={"100%"}
+                  animation="wave"
+                />
+              </Box>
 
-                <Box bgcolor={"surface.1"}>
-                  <CardTemplatePlaceholder count={3} />
-                </Box>
-              </>
-            ) : (
-              <Stack gap={4}>
-                <Typography
-                  fontSize={"24px"}
-                  fontWeight={500}
-                  color={"onSurface"}
-                  lineHeight={"34.32px"}
-                  letterSpacing={"0.17"}
-                >
-                  Automation
-                </Typography>
-                <Stack gap={3}>
-                  {workflows ? (
-                    workflows.map(workflow => (
-                      <CardWorkflow
-                        key={workflow.id}
-                        workflow={workflow}
-                      />
-                    ))
-                  ) : (
-                    <Typography
-                      sx={{
-                        color: "onSurface",
-                        opacity: 0.5,
-                        textAlign: "center",
-                        mt: "50px",
-                      }}
-                    >
-                      No works found
-                    </Typography>
-                  )}
-                </Stack>
+              <Box bgcolor={"surface.1"}>
+                <CardTemplatePlaceholder count={3} />
+              </Box>
+            </>
+          ) : (
+            <Stack gap={4}>
+              <Typography
+                fontSize={"24px"}
+                fontWeight={500}
+                color={"onSurface"}
+                lineHeight={"34.32px"}
+                letterSpacing={"0.17"}
+              >
+                Automation
+              </Typography>
+              <Stack gap={3}>
+                {workflows ? (
+                  workflows.map(workflow => (
+                    <CardWorkflow
+                      key={workflow.id}
+                      workflow={workflow}
+                    />
+                  ))
+                ) : (
+                  <Typography
+                    sx={{
+                      color: "onSurface",
+                      opacity: 0.5,
+                      textAlign: "center",
+                      mt: "50px",
+                    }}
+                  >
+                    No works found
+                  </Typography>
+                )}
               </Stack>
-            )}
-          </Grid>
-        </Box>
-      </Layout>
-    </Protected>
+            </Stack>
+          )}
+        </Grid>
+      </Box>
+    </Layout>
   );
 };
 
-export async function getServerSideProps({ params }: any) {
-  return {
-    props: {
-      title: "Promptify | Boost Your Creativity",
-      description:
-        "Free AI Writing App for Unique Idea & Inspiration. Seamlessly bypass AI writing detection tools, ensuring your work stands out.",
-    },
-  };
+export async function getStaticProps() {
+  return { props: {} };
 }
 
-export default Sparks;
+export default Automation;
