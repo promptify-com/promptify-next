d93ecff186039f546ab811d742606a1948379448
nannuflay <mohameddcharafi@gmail.com>
2024-01-24 14:19:31 +0100

    Add Type Parameter to Differentiate Between Automation and Template Pages

--- src/components/Prompt/Hooks/useVariant.ts
+++ src/components/Prompt/Hooks/useVariant.ts
@@ -90,7 +90,7 @@ const useVariant = () => {
     });
   };
 
-  return { switchVariant, variant, isVariantA, isVariantB };
+  return { switchVariant, variant, isVariantA, isVariantB, isAutomationPage };
 };
 
 export default useVariant;
