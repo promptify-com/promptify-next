070ad6305eab315878dab6b911198ca34df43dcd
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-01-30 09:39:05 +0100

    chore: improve retry mechanism to avoid potential self DDOS

--- src/core/api/axios-base-query.ts
+++ src/core/api/axios-base-query.ts
@@ -1,18 +1,95 @@
-import axios, { AxiosError, type AxiosRequestConfig } from "axios";
+import axios, { AxiosError, type AxiosRequestConfig, type ResponseType } from "axios";
 import { BaseQueryFn } from "@reduxjs/toolkit/dist/query";
 
 import useToken from "@/hooks/useToken";
 import RetryRequestError from "./errors/RetryRequestError";
-import type { ResponseType } from "./dto/templates";
 
 const IGNORED_RESPONSE_STATUS_CODES = [401, 403, 404];
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const attemptRequest = async ({
+  attempt = 0,
+  url,
+  responseType,
+  baseUrl,
+  method,
+  data,
+  params,
+  headers,
+  maxRetries,
+  retryDelay,
+}: {
+  attempt: number;
+  url: string;
+  responseType?: ResponseType;
+  baseUrl: string;
+  method?: string;
+  data: any;
+  params: any;
+  headers: any;
+  maxRetries: number;
+  retryDelay: number;
+}): Promise<{ data: any } | { error: any }> => {
+  try {
+    const pathname = url.endsWith("/") || url.includes("?") ? url : `${url}/`;
+    const axiosReq: AxiosRequestConfig = {
+      url: baseUrl + pathname,
+      method,
+      data,
+      params,
+      headers,
+    };
+
+    if (responseType) {
+      axiosReq.responseType = responseType;
+    }
+
+    if (attempt !== 0) {
+      await sleep(retryDelay * attempt);
+    }
+
+    const result = await axios(axiosReq);
+
+    return { data: result.data };
+  } catch (error) {
+    const _err = error as AxiosError;
+
+    if (_err.response?.status && IGNORED_RESPONSE_STATUS_CODES.includes(_err.response.status)) {
+      throw error;
+    }
+
+    if (attempt < maxRetries) {
+      return attemptRequest({
+        attempt: attempt + 1,
+        url,
+        responseType,
+        baseUrl,
+        method,
+        data,
+        params,
+        headers,
+        maxRetries,
+        retryDelay,
+      });
+    }
+
+    throw new RetryRequestError("Request failed after all retry attempts.");
+  }
+};
+
 export const axiosBaseQuery =
-  (
-    { baseUrl, maxRetries = 3 }: { baseUrl: string; maxRetries?: number } = {
-      baseUrl: "",
-    },
-  ): BaseQueryFn<
+  ({
+    baseUrl = "",
+    maxRetries = 3,
+    retryDelay = 1000,
+  }: {
+    baseUrl: string;
+    maxRetries?: number;
+    retryDelay?: number;
+  }): BaseQueryFn<
     {
       url: string;
       method: AxiosRequestConfig["method"];
@@ -35,41 +112,19 @@ export const axiosBaseQuery =
       headers["Content-Type"] = "application/json";
     }
 
-    const attemptRequest = async (attempt = 0): Promise<{ data: any } | { error: any }> => {
-      try {
-        const properUrl = url.endsWith("/") || url.includes("?") ? url : `${url}/`;
-        const axiosReq: AxiosRequestConfig = {
-          url: baseUrl + properUrl,
-          method,
-          data,
-          params,
-          headers,
-        };
-
-        if (responseType) {
-          axiosReq.responseType = responseType;
-        }
-
-        const result = await axios(axiosReq);
-
-        return { data: result.data };
-      } catch (error) {
-        const _err = error as AxiosError;
-
-        if (_err.response?.status && IGNORED_RESPONSE_STATUS_CODES.includes(_err.response.status)) {
-          throw error;
-        }
-
-        if (attempt < maxRetries) {
-          return attemptRequest(attempt + 1);
-        }
-
-        throw new RetryRequestError("Request failed after all retry attempts.");
-      }
-    };
-
     try {
-      return await attemptRequest();
+      return await attemptRequest({
+        attempt: 0,
+        url,
+        responseType,
+        baseUrl,
+        method,
+        data,
+        params,
+        headers,
+        maxRetries,
+        retryDelay,
+      });
     } catch (axiosError) {
       const err = axiosError as AxiosError;
       const isRetryRequestError = err instanceof RetryRequestError;
