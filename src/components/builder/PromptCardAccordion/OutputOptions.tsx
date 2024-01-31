2580eb945694b83c2cb87f9ac101f5990184021f
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-01-31 21:28:25 +0100

    chore: add missing files and folders and fix some issues

--- src/components/builder/PromptCardAccordion/OutputOptions.tsx
+++ src/components/builder/PromptCardAccordion/OutputOptions.tsx
@@ -31,8 +31,12 @@ function OutputOptions({ prompt, onSave, onCancel }: Props) {
 
   const initialState: IEditPrompts = {
     ...prompt,
-    output_format: initialOutputFormatMatching?.toLowerCase() || "",
-    custom_output_format: initialOutputFormatMatching ? "" : initialOutputFormat,
+    output_format: initialOutputFormatMatching
+      ? initialOutputFormatMatching.toLowerCase()
+      : initialOutputFormat
+      ? "custom"
+      : "",
+    custom_output_format: initialOutputFormat ?? "",
   };
   const [promptData, setPromptData] = useState<IEditPrompts>(initialState);
   const [hasError, setHasError] = useState(false);
