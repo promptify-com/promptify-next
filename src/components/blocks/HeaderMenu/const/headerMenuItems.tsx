6b9573b7654bd6c33de90435c84b9754c07ef175
SALMAN <elfadili.salman@gmail.com>
2024-02-02 17:53:53 +0100

    fix: profile settings identity section (#681)
    * fix: profile settings identity section
    
    * namings correction
    
    * improve IdentityItem
    
    * naming corrections
    
    * revert ts eslint disabled line
    
    * rename Identity file
    
    * rename Identity.tsx
    
    * chore: recreate user icon file
    
    * chore: recreate prompt icon file
    
    ---------
    
    Co-authored-by: Yassine Belkaid <yassine.belkaid87@gmail.com>

--- src/components/blocks/HeaderMenu/const/headerMenuItems.tsx
+++ src/components/blocks/HeaderMenu/const/headerMenuItems.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { Home } from '../../../../assets/icons/Home';
-import { Connection } from '../../../../assets/icons/Connection';
-import { User } from '../../../../assets/icons/user';
-import { Prompt } from '../../../../assets/icons/prompts';
+import React from "react";
+import { Home, Connection, User, Prompt } from "@/assets/icons";
 
 export const headerMenuItems = [
-  { name: 'Home ', icon: <Home />, id: 0, link: 'home' },
-  { name: 'Connections ', icon: <Connection />, id: 1, link: 'connections' },
-  { name: 'Identity Setup ', icon: <User />, id: 2, link: 'identy' },
+  { name: "Home ", icon: <Home />, id: 0, link: "home" },
+  { name: "Connections ", icon: <Connection />, id: 1, link: "connections" },
+  { name: "Identity Setup ", icon: <User />, id: 2, link: "Identity" },
   // { name: 'Data Privacy ', icon: <Code />, id: 3, link: 'privacy' },
   // { name: 'My Content ', icon: <Code />, id: 4, link: 'content' },
-  { name: 'Prompts ', icon: <Prompt />, id: 6, link: 'prompts' },
+  { name: "Prompts ", icon: <Prompt />, id: 6, link: "prompts" },
   // { name: 'Payment Info ', icon: <Code />, id: 5, link: 'payment' },
 ];
 
 export const headerMenuItemsMobile = [
-  { name: 'Home ', icon: <Home />, id: 0, link: 'home' },
-  { name: 'Connections ', icon: <Connection />, id: 1, link: 'connections' },
-  { name: 'Identity Setup ', icon: <User />, id: 2, link: 'identy' },
+  { name: "Home ", icon: <Home />, id: 0, link: "home" },
+  { name: "Connections ", icon: <Connection />, id: 1, link: "connections" },
+  { name: "Identity Setup ", icon: <User />, id: 2, link: "Identity" },
   // { name: 'Data Privacy ', icon: <Code />, id: 3, link: 'privacy' },
   // { name: 'My Content ', icon: <Code />, id: 4, link: 'content' },
   // { name: 'Prompts ', icon: <Prompt />, id: 6, link: 'prompts' },
