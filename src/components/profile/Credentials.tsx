df58df0c49e7d7b39f1aed60117ab626e116b9b7
SALMAN <elfadili.salman@gmail.com>
2024-02-28 12:44:05 +0100

    delete credential flow

--- src/components/profile/Credentials.tsx
+++ src/components/profile/Credentials.tsx
@@ -2,14 +2,23 @@ import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-
 import { formatDate } from "@/common/helpers/timeManipulation";
 import useCredentials from "@/components/Automation/Hooks/useCredentials";
 import type { ICredential } from "@/components/Automation/types";
+import { useDeleteCredentialMutation } from "@/core/api/workflows";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import IconButton from "@mui/material/IconButton";
+import Delete from "@mui/icons-material/Delete";
 
 function Credentials() {
-  const { initializeCredentials } = useCredentials();
-  const [credentials, setCredentials] = useState<ICredential[]>([]);
+  const [selectedCredential, setSelectedCredential] = useState<ICredential | null>(null);
+  const [deleteCredential] = useDeleteCredentialMutation();
+
+  const { credentials, setCredentials, initializeCredentials, removeCredential } = useCredentials();
 
   useEffect(() => {
     // if credentials already in local storage, no http call will be triggered, we're safe here.
@@ -21,6 +30,14 @@ function Credentials() {
       }
     });
   }, []);
+
+  const handleDelete = async () => {
+    if (!selectedCredential) return;
+
+    await deleteCredential(selectedCredential.id);
+    removeCredential(selectedCredential.id);
+    setSelectedCredential(null);
+  };
 
   if (!credentials.length) {
     return;
@@ -106,9 +123,63 @@ function Credentials() {
                 {formatDate(cred.createdAt)}
               </Typography>
             </Grid>
+            {cred.type !== "promptifyApi" && (
+              <Grid
+                ml={2}
+                item
+              >
+                <IconButton
+                  onClick={() => setSelectedCredential(cred)}
+                  sx={{
+                    border: "none",
+                    color: "onSurface",
+                    "&:hover": {
+                      color: "#ef4444",
+                    },
+                  }}
+                >
+                  <Delete />
+                </IconButton>
+              </Grid>
+            )}
           </Grid>
         </Box>
       ))}
+      {selectedCredential?.id && (
+        <Dialog
+          open
+          disableScrollLock={true}
+          onClose={() => setSelectedCredential(null)}
+        >
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to remove <b>{selectedCredential.name}</b>?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions
+            sx={{
+              p: "8px 15px 15px",
+            }}
+          >
+            <Button
+              sx={{
+                "&:hover": {
+                  backgroundColor: "grey.300",
+                },
+              }}
+              onClick={() => setSelectedCredential(null)}
+            >
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleDelete}
+            >
+              Remove
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </Box>
   );
 }
