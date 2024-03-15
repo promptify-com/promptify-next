bd2a1785046275331828845487f84a84aa877c15
nannuflay <mohameddcharafi@gmail.com>
2024-03-15 14:10:33 +0000

    fix messagesList types structure

--- src/core/api/dto/chats.ts
+++ src/core/api/dto/chats.ts
@@ -24,18 +24,37 @@ interface IMessageResult {
   updated_at: string;
   created_at: string;
   message_type: "message" | "suggestion" | "execution" | "template";
-  message_object: {
-    id: number;
-    created_at: string;
-    updated_at: string;
-    text: string; // for inputs
-    type: string; // for inputs
-    templates: Templates[];
+  message_object: InputMessage | SuggestionsMessage | ExecutionMessage | TemplateMessage;
+}
 
-    execution: TemplatesExecutions;
+interface InputMessage {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  text: string;
+  type: string;
+}
 
-    template: Templates;
-  };
+interface SuggestionsMessage {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  templates: Templates[];
+}
+
+interface ExecutionMessage {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  execution: TemplatesExecutions;
+}
+
+interface TemplateMessage {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  text: string;
+  template: Templates;
 }
 
 export interface IMessageList {
