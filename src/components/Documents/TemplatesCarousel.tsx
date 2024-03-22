81d28c5c69c5de2d6f426322ae70784c1f992b93
SALMAN <elfadili.salman@gmail.com>
2024-03-22 01:20:31 +0000

    templates carousel see all listing

--- src/components/Documents/TemplatesCarousel.tsx
+++ src/components/Documents/TemplatesCarousel.tsx
@@ -1,6 +1,4 @@
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import CarouselButtons from "@/components/common/buttons/CarouselButtons";
@@ -8,8 +6,9 @@ import useCarousel from "@/hooks/useCarousel";
 import CardDocumentTemplate from "@/components/common/cards/CardDocumentTemplate";
 import type { TemplateExecutionsDisplay } from "@/core/api/dto/templates";
 import CardDocumentTemplatePlaceholder from "@/components/placeholders/CardDocumentTemplatePlaceholder";
-import TemplatesMenuSection from "./TemplatesMenu";
 import { useState } from "react";
+import { Grid } from "@mui/material";
+import { useAppSelector } from "@/hooks/useStore";
 
 interface Props {
   templates: TemplateExecutionsDisplay[] | undefined;
@@ -18,7 +17,9 @@ interface Props {
 
 export default function TemplatesCarousel({ templates, isLoading }: Props) {
   const { containerRef: carouselRef, scrollNext, scrollPrev } = useCarousel();
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [isCarousel, setIsCarousel] = useState(true);
+
+  const isDocumentsFiltersSticky = useAppSelector(state => state.sidebar.isDocumentsFiltersSticky);
 
   const sortedTemplates = templates?.slice().sort((tempA, tempB) => tempB.executions.length - tempA.executions.length);
 
@@ -41,56 +42,64 @@ export default function TemplatesCarousel({ templates, isLoading }: Props) {
         >
           Top prompts
         </Typography>
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-          gap={1}
-          sx={{ display: { xs: "none", md: "flex" } }}
-        >
-          <Button
-            onClick={e => setAnchorEl(e.currentTarget)}
-            variant="outlined"
-            sx={{ color: "#67677C", visibility: isLoading ? "hidden" : "visible" }}
+        {isCarousel && (
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            gap={1}
+            sx={{ display: { xs: "none", md: "flex" } }}
           >
-            See all
-          </Button>
-          <CarouselButtons
-            scrollPrev={scrollPrev}
-            scrollNext={scrollNext}
-            canScrollNext={true}
-            canScrollPrev={true}
-          />
-        </Stack>
+            <Button
+              onClick={e => setIsCarousel(false)}
+              variant="outlined"
+              sx={{ color: "#67677C", visibility: isLoading ? "hidden" : "visible" }}
+            >
+              See all
+            </Button>
+            <CarouselButtons
+              scrollPrev={scrollPrev}
+              scrollNext={scrollNext}
+              canScrollNext={true}
+              canScrollPrev={true}
+            />
+          </Stack>
+        )}
       </Stack>
       <Stack
-        ref={carouselRef}
+        ref={isCarousel ? carouselRef : null}
         overflow={"hidden"}
       >
-        <Stack
-          direction={"row"}
+        <Grid
+          container
           gap={isLoading ? 3 : 0}
+          rowGap={2}
+          flexWrap={isCarousel ? "nowrap" : "wrap"}
+          {...(!isCarousel && {
+            sx: {
+              transform: "none !important",
+            },
+          })}
         >
           {isLoading ? (
             <CardDocumentTemplatePlaceholder count={5} />
           ) : (
             sortedTemplates?.map(template => (
-              <Box key={template.id}>
+              <Grid
+                key={template.id}
+                item
+                xs={12}
+                sm={6}
+                md={isDocumentsFiltersSticky ? 8 : 6}
+                lg={isDocumentsFiltersSticky ? 5 : 3}
+                xl={2}
+              >
                 <CardDocumentTemplate template={template} />
-              </Box>
+              </Grid>
             ))
           )}
-        </Stack>
+        </Grid>
       </Stack>
-
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={() => setAnchorEl(null)}
-        disableScrollLock
-      >
-        <TemplatesMenuSection templates={sortedTemplates || []} />
-      </Menu>
     </Stack>
   );
 }
