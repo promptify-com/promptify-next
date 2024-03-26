7b79e8303aca314d61b98559fa819e84f6a912fc
nannuflay <mohameddcharafi@gmail.com>
2024-03-26 18:52:26 +0000

    remove console

--- src/pages/prompt-builder/create/index.tsx
+++ src/pages/prompt-builder/create/index.tsx
@@ -5,8 +5,6 @@ import { useSearchParams } from "next/navigation";
 function CreatePage() {
   const searchParams = useSearchParams();
 
-  console.log("get", searchParams.get("editor"), "has");
-
   return <Layout>{searchParams.has("editor") ? <div>loading...</div> : <WelcomeScreen />}</Layout>;
 }
 
