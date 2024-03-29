15c65cbc3ab480b6d0c84fca6bd178ab18a40af2
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-03-29 17:20:28 +0000

    fix: not showing read more if bio is small

--- src/pages/users/[username].tsx
+++ src/pages/users/[username].tsx
@@ -117,17 +117,19 @@ function UserInformation({ username, user }: { username: string | undefined; use
           {truncateBio ? (
             <>
               {truncate(user.bio, { length: 190 })}
-              <Typography
-                component={"span"}
-                sx={{
-                  fontSize: 15,
-                  fontWeight: 500,
-                  cursor: "pointer",
-                }}
-                onClick={() => setTruncateBio(!truncateBio)}
-              >
-                Read more
-              </Typography>
+              {user.bio?.length > 190 && (
+                <Typography
+                  component={"span"}
+                  sx={{
+                    fontSize: 15,
+                    fontWeight: 500,
+                    cursor: "pointer",
+                  }}
+                  onClick={() => setTruncateBio(!truncateBio)}
+                >
+                  Read more
+                </Typography>
+              )}
             </>
           ) : (
             user.bio
@@ -251,18 +253,16 @@ function PromptsList({ username, firstName, lastName }: { username: string; firs
               <Grid
                 container
                 gap={{ xs: 1, sm: 0 }}
-                spacing={1}
-                maxWidth={{ xs: "98%", sm: "auto" }}
               >
                 <>
                   {allTemplates?.map(template => (
                     <Grid
                       key={template.id}
                       item
-                      xs={12}
-                      sm={6}
-                      md={5}
+                      sm={4}
+                      md={4}
                       lg={3}
+                      xl={2}
                     >
                       <CardTemplate
                         template={template}
