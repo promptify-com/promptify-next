e96909cf468cde4e5968884bcf850dfc44cade68
SALMAN <elfadili.salman@gmail.com>
2024-04-02 00:04:35 +0000

    prompt execution params accordion section

--- src/components/Documents/DocumentPage/Details.tsx
+++ src/components/Documents/DocumentPage/Details.tsx
@@ -2,6 +2,24 @@ import Stack from "@mui/material/Stack";
 import type { ExecutionWithTemplate } from "@/core/api/dto/templates";
 import Typography from "@mui/material/Typography";
 import Image from "@/components/design-system/Image";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { theme } from "@/theme";
+import Link from "next/link";
+import Edit from "@mui/icons-material/Edit";
+
+const PARAMETERS = [
+  { name: "Age", value: 30 },
+  { name: "Gender", value: "male" },
+  { name: "Purpose", value: "maintain my weight and this is long text item" },
+  { name: "Activity level", value: "none" },
+  { name: "Equipment", value: "none" },
+  { name: "Specific requirement", value: "none" },
+];
 
 interface Props {
   document: ExecutionWithTemplate;
@@ -36,7 +54,7 @@ function Details({ document }: Props) {
           <Image
             src={template.thumbnail ?? require("@/assets/images/default-thumbnail.jpg")}
             alt={template.title}
-            style={{ borderRadius: "24px", objectFit: "cover", width: "103px", height: "100%" }}
+            style={{ borderRadius: "24px", objectFit: "cover", width: "103px", height: "77px" }}
           />
           <Stack
             flex={1}
@@ -51,7 +69,7 @@ function Details({ document }: Props) {
             </Typography>
             <Typography
               fontSize={16}
-              fontWeight={500}
+              fontWeight={400}
               color={"secondary.light"}
             >
               {template.category.name}
@@ -59,8 +77,89 @@ function Details({ document }: Props) {
           </Stack>
         </Stack>
       </Stack>
+      <Accordion
+        defaultExpanded
+        sx={{
+          boxShadow: "none",
+          px: "8px",
+          borderRadius: "24px !important",
+          bgcolor: "surfaceContainerLow",
+          m: "0 !important",
+          "::before": { display: "none" },
+          ".MuiAccordionSummary-root": {
+            p: "16px 8px",
+          },
+          ".MuiAccordionSummary-content": { m: "0 !important" },
+          ".MuiAccordionDetails-root": { p: 0 },
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMore />}>Prompt instruction:</AccordionSummary>
+        <AccordionDetails>
+          <Stack gap={1}>
+            <Box sx={inputsContainer}>
+              <Typography sx={instructionTitleStyle}>Motivational Coach</Typography>
+              {PARAMETERS.map(param => (
+                <Typography
+                  key={param.name}
+                  fontSize={15}
+                  fontWeight={500}
+                  color={"secondary.main"}
+                  sx={inputStyle}
+                >
+                  {param.name} <span className="val">{param.value}</span>
+                </Typography>
+              ))}
+            </Box>
+            <Box sx={inputsContainer}>
+              <Typography sx={instructionTitleStyle}>Contextual parameters</Typography>
+              <Typography
+                fontSize={15}
+                fontWeight={500}
+                color={"secondary.main"}
+                sx={inputStyle}
+              >
+                Tone <span className="val">Professional</span>
+              </Typography>
+            </Box>
+            <Button
+              LinkComponent={Link}
+              href={`/prompt-builder/${template.slug}`}
+              target="_blank"
+              endIcon={<Edit />}
+              variant="contained"
+              sx={{
+                my: "8px",
+                p: "8px 16px",
+                fontSize: 14,
+                fontWeight: 500,
+                svg: {
+                  fonSize: 24,
+                },
+              }}
+            >
+              Edit
+            </Button>
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
     </Stack>
   );
 }
 
 export default Details;
+
+const instructionTitleStyle = {
+  fontSize: 13,
+  fontWeight: 400,
+  color: "onSurface",
+  p: "16px 16px 8px",
+};
+const inputsContainer = {
+  bgcolor: "surfaceContainerLowest",
+  borderRadius: "24px",
+};
+const inputStyle = {
+  p: "12px 16px",
+  ".val": { fontWeight: 400 },
+  ":not(:last-of-type)": { borderBottom: `1px solid ${theme.palette.surfaceContainerHigh}` },
+};
