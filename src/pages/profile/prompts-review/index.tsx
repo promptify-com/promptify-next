566d1c44add1ca3803ab67e873fac99a6ff760a3
SadHR <saad.harakat5@gmail.com>
2024-04-03 10:49:30 +0000

    Refactoring My Prompts.

--- src/pages/profile/prompts-review/index.tsx
+++ src/pages/profile/prompts-review/index.tsx
@@ -1,26 +1,20 @@
-import { Layout } from "@/layout";
+import { useState } from "react";
 
 import Protected from "@/components/Protected";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import TemplateCard from "@/components/common/TemplateCard";
-import Button from "@mui/material/Button";
-import Add from "@mui/icons-material/Add";
-import Link from "next/link";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Typography from "@mui/material/Typography";
-import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
-import TemplatesPaginatedList from "@/components/TemplatesPaginatedList";
 import ArrowRight from "@mui/icons-material/ArrowRight";
+import TemplatesPaginatedList from "@/components/TemplatesPaginatedList";
+import PromptsSort from "@/components/profile2/MyPrompts/PromptsSort";
+import PromptsContainer from "@/components/profile2/MyPrompts/PromptsContainer";
 
+import { Layout } from "@/layout";
 import { SEO_DESCRIPTION } from "@/common/constants";
-import type { Templates } from "@/core/api/dto/templates";
-import { useState } from "react";
-import { SortOption } from "@/components/profile2/Types";
 import { SORTING_OPTIONS } from "@/components/profile2/Constants";
 import { useGetTemplatesByFilter } from "@/hooks/useGetTemplatesByFilter";
-import TemplateCardPlaceholder from "@/components/placeholders/TemplateCardPlaceholder";
+import { SortOption } from "@/components/profile2/Types";
+import type { Templates } from "@/core/api/dto/templates";
 
 function ProfilePromptsReview() {
   const [sortOption, setSortOption] = useState(SORTING_OPTIONS[0]);
@@ -49,133 +43,54 @@ function ProfilePromptsReview() {
   return (
     <Protected>
       <Layout>
-        <Stack
-          gap={5}
-          sx={{
-            maxWidth: "1184px",
-            width: "85%",
-            m: "auto",
-            p: "40px 20px",
-          }}
+        <PromptsContainer
+          title="Prompts Review"
+          sortOption={sortOption}
+          setSortAnchor={setSortAnchor}
+          sortOpen={sortOpen}
+          isFetching={isFetching}
         >
-          <Stack
-            direction={"row"}
-            alignItems={"center"}
-            justifyContent={"space-between"}
-            gap={1}
-            p={"8px 16px"}
+          <TemplatesPaginatedList
+            loading={isFetching}
+            hasNext={!!hasMore}
+            onNextPage={handleNextPage}
+            hasPrev={hasPrev}
+            onPrevPage={handlePrevPage}
+            endIcon={<ArrowRight />}
           >
-            <Typography
-              fontSize={32}
-              fontWeight={400}
-              color={"onSurface"}
-            >
-              Prompts Review
-            </Typography>
             <Stack
-              direction={"row"}
-              alignItems={"center"}
-              gap={1}
+              alignItems={"flex-start"}
+              gap={2}
+              px={"16px"}
             >
-              <Button
-                onClick={e => setSortAnchor(e.currentTarget)}
-                endIcon={<ArrowDropDown />}
-                sx={{
-                  border: "1px solid",
-                  borderColor: "surfaceContainerHigh",
-                  p: "8px 16px",
-                  fontSize: 14,
-                  fontWeight: 500,
-                  color: "onSurface",
-                  gap: 0.5,
-                  label: { color: "secondary.light", cursor: "pointer" },
-                  svg: {
-                    transform: sortOpen ? "rotateX(180deg)" : "none",
-                  },
-                  ":hover": {
-                    bgcolor: "surfaceContainerHigh",
-                  },
-                }}
-              >
-                <Box component={"label"}>Sort by:</Box>
-                {sortOption.label}
-              </Button>
-              <Button
-                LinkComponent={Link}
-                href="/prompt-builder/create"
-                variant="contained"
-                endIcon={<Add />}
-              >
-                New prompt
-              </Button>
+              {templates?.map(template => (
+                <Box
+                  key={template.id}
+                  sx={{
+                    width: "100%",
+                    border: "1px solid",
+                    borderColor: "surfaceContainerHighest",
+                    borderRadius: "16px",
+                  }}
+                >
+                  <TemplateCard
+                    template={template}
+                    manageActions
+                    displayAvatar
+                  />
+                </Box>
+              ))}
             </Stack>
-          </Stack>
+          </TemplatesPaginatedList>
+        </PromptsContainer>
 
-          {isFetching ? (
-            <TemplateCardPlaceholder />
-          ) : (
-            <TemplatesPaginatedList
-              loading={isFetching}
-              hasNext={!!hasMore}
-              onNextPage={handleNextPage}
-              hasPrev={hasPrev}
-              onPrevPage={handlePrevPage}
-              endIcon={<ArrowRight />}
-            >
-              <Stack
-                alignItems={"flex-start"}
-                gap={2}
-                px={"16px"}
-              >
-                {templates?.map(template => (
-                  <Box
-                    key={template.id}
-                    sx={{
-                      width: "100%",
-                      border: "1px solid",
-                      borderColor: "surfaceContainerHighest",
-                      borderRadius: "16px",
-                    }}
-                  >
-                    <TemplateCard
-                      template={template}
-                      manageActions
-                      displayAvatar
-                    />
-                  </Box>
-                ))}
-              </Stack>
-            </TemplatesPaginatedList>
-          )}
-        </Stack>
-
-        <Menu
-          anchorEl={sortAnchor}
-          open={sortOpen}
-          onClose={() => setSortAnchor(null)}
-          disableScrollLock
-          sx={{
-            ".MuiList-root": {
-              p: 0,
-            },
-            ".MuiMenuItem-root": {
-              borderTop: "1px solid #E3E3E3",
-              fontSize: 14,
-              fontWeight: 400,
-              color: "onSurface",
-            },
-          }}
-        >
-          {SORTING_OPTIONS.map(option => (
-            <MenuItem
-              key={option.label}
-              onClick={() => handleSelectSort(option)}
-              disabled={sortOption.orderby === option.orderby}
-            >
-              {option.label}
-            </MenuItem>
-          ))}
-        </Menu>
+        <PromptsSort
+          sortAnchor={sortAnchor}
+          sortOpen={sortOpen}
+          setSortAnchor={setSortAnchor}
+          handleSelectSort={handleSelectSort}
+          sortOption={sortOption}
+        />
       </Layout>
     </Protected>
   );
