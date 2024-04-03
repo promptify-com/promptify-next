a46bee3588f7d05028fc6cf7c054f60af623a286
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-04-03 16:01:01 +0000

    fix: display chat option in prefs

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+        "@typescript-eslint/non-nullable-type-assertion-style": "off",
 
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": "warn",
