7d6f88a8ca721812611fedbe31cd1838c35ef7e1
SALMAN <elfadili.salman@gmail.com>
2024-04-05 15:28:02 +0000

    use SwipeableDrawer for sidebar drawer

--- src/components/sidebar/DrawerContainer.tsx
+++ src/components/sidebar/DrawerContainer.tsx
@@ -2,50 +2,14 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import MuiDrawer from "@mui/material/Drawer";
-import { styled, type Theme, type CSSObject, useTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import type { ReactNode } from "react";
 import { KeyboardTab } from "@mui/icons-material";
 import useBrowser from "@/hooks/useBrowser";
 import Close from "@mui/icons-material/Close";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 
 const drawerWidth = 255;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `140px`,
-  [theme.breakpoints.up("sm")]: {
-    width: `86px`,
-  },
-});
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: prop => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
 
 interface Props {
   title: string;
@@ -60,10 +24,13 @@ export default function DrawerContainer({ title, expanded, toggleExpand, sticky,
   const { isMobile } = useBrowser();
 
   return (
-    <Drawer
-      variant={"permanent"}
+    <SwipeableDrawer
       anchor="left"
+      variant={isMobile ? "temporary" : "permanent"}
+      disablePortal
       open={expanded}
+      onClose={() => {}}
+      onOpen={() => {}}
       sx={{
         zIndex: 1,
         alignItems: "center",
@@ -74,7 +41,6 @@ export default function DrawerContainer({ title, expanded, toggleExpand, sticky,
           width: { xs: "85svw", md: `calc(${theme.custom.leftClosedSidebarWidth} + ${drawerWidth}px)` },
           left: { xs: 0, md: theme.custom.leftClosedSidebarWidth },
           mt: { xs: theme.custom.headerHeight.xs, md: 0 },
-          boxShadow: { xs: "0px 0px 24px 8px rgba(0, 0, 0, 0.1), 0px 2px 16px 0px rgba(0, 0, 0, 0.1)", md: "none" },
           my: 0,
           padding: "10px 20px",
           borderRadius: 0,
@@ -128,6 +94,6 @@ export default function DrawerContainer({ title, expanded, toggleExpand, sticky,
       </Stack>
       <Divider />
       {children}
-    </Drawer>
+    </SwipeableDrawer>
   );
 }
