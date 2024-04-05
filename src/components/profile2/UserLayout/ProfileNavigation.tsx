a34c7619b9db746a633eb6d834e3c8fae990ec1f
SadHR <saad.harakat5@gmail.com>
2024-04-05 19:35:58 +0000

    adjust the button.

--- src/components/profile2/UserLayout/ProfileNavigation.tsx
+++ src/components/profile2/UserLayout/ProfileNavigation.tsx
@@ -5,7 +5,7 @@ import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRound
 import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
 import Link from "next/link";
 import { navItems } from "@/components/profile2/Constants";
-import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
 
 interface NavItem {
   title: string;
@@ -42,10 +42,10 @@ const ProfileNavigation = () => {
       sx={{
         display: "flex",
         justifyContent: justifyContent,
-        alignItems: "flex-start",
+        alignItems: "center",
         gap: "16px",
         alignSelf: "stretch",
-        m: "20px",
+        p: "16px",
       }}
     >
       {prevItem && (
@@ -54,15 +54,16 @@ const ProfileNavigation = () => {
           passHref
           style={{
             textDecoration: "none",
+            width: "50%",
           }}
         >
-          <Button
+          <IconButton
             component="a"
             sx={btnStyle}
-            startIcon={<ArrowBackIosNewRoundedIcon sx={{ color: "onSurface" }} />}
           >
+            <ArrowBackIosNewRoundedIcon sx={{ color: "onSurface" }} />
             <Typography sx={btnTypographyStyle}>{prevItem?.title}</Typography>
-          </Button>
+          </IconButton>
         </Link>
       )}
       {nextItem && (
@@ -71,15 +72,16 @@ const ProfileNavigation = () => {
           passHref
           style={{
             textDecoration: "none",
+            width: "50%",
           }}
         >
-          <Button
+          <IconButton
             component="a"
             sx={btnStyle}
-            endIcon={<ArrowForwardIosRoundedIcon sx={{ color: "onSurface" }} />}
           >
             <Typography sx={btnTypographyStyle}>{nextItem?.title}</Typography>
-          </Button>
+            <ArrowForwardIosRoundedIcon sx={{ color: "onSurface" }} />
+          </IconButton>
         </Link>
       )}
     </Box>
@@ -97,25 +99,19 @@ const btnStyle = {
   border: "1px solid",
   borderColor: "surfaceContainerHigh",
   textDecoration: "none",
+  width: "100%",
 };
 
 const btnTypographyStyle = {
   color: "onSurface",
-  fontFeatureSettings: "'clig' off, 'liga' off",
   fontFamily: "Poppins",
   fontSize: "16px",
-  fontStyle: "normal",
   fontWeight: 500,
   lineHeight: "150%",
-  display: "inline",
-  minWidth: "100%",
-  whiteSpace: "nowrap",
+  display: "block",
   overflow: "hidden",
   textOverflow: "ellipsis",
-  "&:hover": {
-    color: "onSurface",
-    borderColor: "onSurface",
-  },
+  whiteSpace: "nowrap",
 };
 
 export default ProfileNavigation;
