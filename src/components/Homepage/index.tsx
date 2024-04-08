7ed35fedd0b1cb1931667da817650d68059a8ad0
yassine belkaid <yassine.belkaid87@gmail.com>
2024-04-08 14:00:43 +0000

    fix: microsoft signin (#826)

--- src/components/Homepage/index.tsx
+++ src/components/Homepage/index.tsx
@@ -2,71 +2,17 @@ import { useGetTemplatesSuggestedQuery } from "@/core/api/templates";
 import { Category } from "@/core/api/dto/templates";
 import { TemplatesSection } from "@/components/explorer/TemplatesSection";
 import Typography from "@mui/material/Typography";
-import { useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import ClientOnly from "@/components/base/ClientOnly";
-import { useAppDispatch, useAppSelector } from "@/hooks/useStore";
+import { useAppSelector } from "@/hooks/useStore";
 import { useGetLatestExecutedTemplatesQuery } from "@/core/api/executions";
-import { userApi } from "@/core/api/user";
-import { AxiosResponse } from "axios";
-import { IContinueWithSocialMediaResponse } from "@/common/types";
-import { getPathURL, saveToken } from "@/common/utils";
-import { updateUser } from "@/core/store/userSlice";
-import { redirectToPath } from "@/common/helpers";
-import { client } from "@/common/axios";
 import { CategoriesSection } from "@/components/explorer/CategoriesSection";
 
-const CODE_TOKEN_ENDPOINT = "/api/login/social/token/";
-
 function HomepageLayout({ categories }: { categories: Category[] }) {
-  const path = getPathURL();
-  const dispatch = useAppDispatch();
   const currentUser = useAppSelector(state => state.user.currentUser);
-  const [getCurrentUser] = userApi.endpoints.getCurrentUser.useLazyQuery();
   const { data: myLatestExecutions, isLoading: isMyLatestExecutionsLoading } =
     useGetLatestExecutedTemplatesQuery(undefined);
   const { data: suggestedTemplates, isLoading: isSuggestedTemplateLoading } = useGetTemplatesSuggestedQuery(undefined);
-
-  // TODO: move authentication logic to signin page instead
-  const doPostLogin = async (response: AxiosResponse<IContinueWithSocialMediaResponse>) => {
-    if (typeof response.data !== "object" || response.data === null) {
-      console.error("incoming data for Microsoft authentication is not an object:", response.data);
-      return;
-    }
-
-    const { token: _token } = response.data;
-
-    if (!_token) {
-      console.error("incoming token for Microsoft authentication is not present:", _token);
-      return;
-    }
-
-    saveToken({ token: _token });
-    const payload = await getCurrentUser(_token).unwrap();
-
-    dispatch(updateUser(payload));
-    redirectToPath(path || "/");
-  };
-
-  // TODO: move authentication logic to signin page instead
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location.search);
-    const authorizationCode = urlParams.get("code");
-
-    if (!!authorizationCode) {
-      client
-        .post(CODE_TOKEN_ENDPOINT, {
-          provider: "microsoft",
-          code: authorizationCode,
-        })
-        .then((response: AxiosResponse<IContinueWithSocialMediaResponse>) => {
-          doPostLogin(response);
-        })
-        .catch(reason => {
-          console.warn("Could not authenticate via Microsoft:", reason);
-        });
-    }
-  }, []);
 
   return (
     <ClientOnly>
