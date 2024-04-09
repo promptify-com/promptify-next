1099d53f64a19ec163ec74c5fd5b94253ab4ad50
yassine belkaid <yassine.belkaid87@gmail.com>
2024-04-09 10:42:54 +0000

    Update cypress/e2e/page_tests.cy.ts

--- cypress/e2e/page_tests.cy.ts
+++ cypress/e2e/page_tests.cy.ts
@@ -1,6 +1,6 @@
 describe("Page guest existence Tests", () => {
   it("Main container of guest page exists", () => {
     cy.visit("/");
-    cy.get('[data-testid="guest-main-container"]').should("exist");
+    cy.get('[data-cy="guest-main-container"]').should("exist");
   });
 });
