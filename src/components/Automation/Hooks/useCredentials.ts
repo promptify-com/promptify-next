5d97420abe6e6dd87a5134cbc9df1c07954f2108
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-04-19 13:23:14 +0100

    chore: fix run workflow and refresh button to recreate credentials

--- src/components/Automation/Hooks/useCredentials.ts
+++ src/components/Automation/Hooks/useCredentials.ts
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import Storage from "@/common/storage";
 import { useAppDispatch, useAppSelector } from "@/hooks/useStore";
-import { workflowsApi } from "@/core/api/workflows";
+import { useUpdateWorkflowMutation, workflowsApi } from "@/core/api/workflows";
 import { extractCredentialsInput } from "@/components/Automation/helpers";
-import { setCredentialsInput } from "@/core/store/chatSlice";
-import type { ICredential, ICredentialInput, INode } from "@/components/Automation/types";
+import { setAreCredentialsStored, setCredentialsInput } from "@/core/store/chatSlice";
+import type {
+  ICredential,
+  ICredentialInput,
+  INode,
+  INodeCredentials,
+  IStoredWorkflows,
+} from "@/components/Automation/types";
 
 const useCredentials = () => {
   const dispatch = useAppDispatch();
   const [credentials, setCredentials] = useState<ICredential[]>(
     (Storage.get("credentials") as unknown as ICredential[]) || [],
   );
-  const credentialsInput = useAppSelector(state => state.chat.credentialsInput);
+  const { credentialsInput, areCredentialsStored } = useAppSelector(state => state.chat);
   const currentUser = useAppSelector(state => state.user.currentUser);
   const [getCredentials] = workflowsApi.endpoints.getCredentials.useLazyQuery();
   const initializeCredentials = (): Promise<ICredential[]> => {
@@ -39,6 +45,8 @@ const useCredentials = () => {
       }
     });
   };
+  const [getWorkflow] = workflowsApi.endpoints.getWorkflow.useLazyQuery();
+  const [updateWorkflow] = useUpdateWorkflowMutation();
 
   async function extractCredentialsInputFromNodes(nodes: INode[]) {
     await initializeCredentials();
@@ -58,7 +66,9 @@ const useCredentials = () => {
   };
 
   const checkCredentialInserted = (credential: ICredentialInput) => {
-    return !!credentials.find(c => c.type === credential?.name);
+    const _credentials = (Storage.get("credentials") || []) as ICredential[];
+
+    return _credentials.findIndex(c => c.type === credential?.name) !== -1;
   };
 
   const updateCredentials = (newCredential: ICredential) => {
@@ -69,10 +79,56 @@ const useCredentials = () => {
   };
 
   const removeCredential = (credentialId: string) => {
-    const updatedCredentials = credentials.filter(credential => credential.id !== credentialId);
+    const _credentials = (Storage.get("credentials") || []) as ICredential[];
+    const updatedCredentials = _credentials.filter(credential => credential.id !== credentialId);
+
     setCredentials(updatedCredentials);
     Storage.set("credentials", JSON.stringify(updatedCredentials));
   };
+
+  function recreateNodeCredentials(nodeCredentials: INodeCredentials) {
+    const _newCredentials = {} as INodeCredentials;
+
+    for (const provider in nodeCredentials) {
+      _newCredentials[provider] = { id: nodeCredentials[provider].id, name: nodeCredentials[provider].name };
+    }
+
+    return _newCredentials;
+  }
+
+  async function updateWorkflowAfterCredentialsDeletion(credentialType: string) {
+    const storedWorkflows = (Storage.get("workflows") as unknown as IStoredWorkflows) || {};
+
+    if (!!Object.values(storedWorkflows).length) {
+      for (const _workflowId in storedWorkflows) {
+        const _workflow = await getWorkflow(storedWorkflows[_workflowId].id).unwrap();
+        let credentialFound = false;
+        const _updatedWorkflow = {
+          ..._workflow,
+          nodes: _workflow.nodes.map(node => ({
+            ...node,
+            ...(node.credentials && { credentials: recreateNodeCredentials(node.credentials) }),
+          })),
+        };
+
+        _updatedWorkflow.nodes.forEach(node => {
+          if (!node.credentials) {
+            return;
+          }
+
+          if (node.credentials[credentialType]) {
+            node.credentials[credentialType].name = "to_be_deleted";
+            credentialFound = true;
+          }
+        });
+
+        if (credentialFound) {
+          await updateWorkflow({ workflowId: Number(_workflowId), data: _updatedWorkflow });
+          dispatch(setAreCredentialsStored(false));
+        }
+      }
+    }
+  }
 
   return {
     credentials,
@@ -84,6 +140,7 @@ const useCredentials = () => {
     credentialsInput,
     extractCredentialsInputFromNodes,
     removeCredential,
+    updateWorkflowAfterCredentialsDeletion,
   };
 };
 
