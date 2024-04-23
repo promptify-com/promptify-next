e4ef18b7f4156c748ca0812b183bbf2c4e4c2ddd
nannuflay <mohameddcharafi@gmail.com>
2024-04-23 11:22:17 +0100

    improve Guest layout optimizations

--- src/components/Homepage/GuestUserLayout/index.tsx
+++ src/components/Homepage/GuestUserLayout/index.tsx
@@ -1,9 +1,16 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
 import { useGetTemplatesByFilterQuery } from "@/core/api/templates";
 import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+import { getPathURL, saveToken } from "@/common/utils";
+import { updateUser } from "@/core/store/userSlice";
+import { redirectToPath } from "@/common/helpers";
+import { client } from "@/common/axios";
+import { useAppDispatch } from "@/hooks/useStore";
+import { userApi } from "@/core/api/user";
+import useBrowser from "@/hooks/useBrowser";
 import Landing from "@/components/Homepage/GuestUserLayout/Landing";
 import CategoryCarousel from "@/components/common/CategoriesCarousel";
 import Services from "@/components/Homepage/GuestUserLayout/Services";
@@ -11,24 +18,25 @@ import Learn from "@/components/Homepage/GuestUserLayout/Learn";
 import Testimonials from "@/components/Homepage/GuestUserLayout/Testimonials";
 import HomepageTemplates from "@/components/Homepage/HomepageTemplates";
 import type { Category } from "@/core/api/dto/templates";
-import { useAppDispatch } from "@/hooks/useStore";
-import { userApi } from "@/core/api/user";
 import type { AxiosResponse } from "axios";
 import type { IContinueWithSocialMediaResponse } from "@/common/types";
-import { getPathURL, saveToken } from "@/common/utils";
-import { updateUser } from "@/core/store/userSlice";
-import { redirectToPath } from "@/common/helpers";
-import { client } from "@/common/axios";
 
 const CODE_TOKEN_ENDPOINT = "/api/login/social/token/";
 
 function GuestUserLayout({ categories }: { categories: Category[] }) {
+  const dispatch = useAppDispatch();
+  const { isMobile } = useBrowser();
+  const path = getPathURL();
+
+  const [getCurrentUser] = userApi.endpoints.getCurrentUser.useLazyQuery();
+
   const templateContainerRef = useRef<HTMLDivElement | null>(null);
   const learnContainerRef = useRef<HTMLDivElement | null>(null);
   const testimonialsContainerRef = useRef<HTMLDivElement | null>(null);
-  const path = getPathURL();
-  const dispatch = useAppDispatch();
-  const [getCurrentUser] = userApi.endpoints.getCurrentUser.useLazyQuery();
+  const carouselContainerRef = useRef<HTMLDivElement | null>(null);
+  const [loadCarousel, setLoadCarousel] = useState(!isMobile); // Initially load if not mobile
+
+  const carouselObserver = useIntersectionObserver(carouselContainerRef, {});
   const observers = {
     templatesObserver: useIntersectionObserver(templateContainerRef, {}),
     learnObserver: useIntersectionObserver(learnContainerRef, {}),
@@ -47,6 +55,13 @@ function GuestUserLayout({ categories }: { categories: Category[] }) {
   const _categories = categories.filter(
     category => !category.parent && category.is_visible && category.prompt_template_count,
   );
+
+  useEffect(() => {
+    if (isMobile && carouselObserver?.isIntersecting) {
+      setLoadCarousel(true);
+    }
+  }, [carouselObserver?.isIntersecting]);
+
   // TODO: move authentication logic to signin page instead
   const doPostLogin = async (response: AxiosResponse<IContinueWithSocialMediaResponse>) => {
     if (typeof response.data !== "object" || response.data === null) {
@@ -97,12 +112,17 @@ function GuestUserLayout({ categories }: { categories: Category[] }) {
       data-cy="guest-main-container"
     >
       <Landing />
-      <CategoryCarousel
-        categories={_categories}
-        href="/explore"
-        autoPlay
-        explore
-      />
+      <Box ref={carouselContainerRef}>
+        {loadCarousel && (
+          <CategoryCarousel
+            categories={_categories}
+            href="/explore"
+            autoPlay
+            explore
+          />
+        )}
+      </Box>
+
       <Services />
       <Stack
         ref={templateContainerRef}
