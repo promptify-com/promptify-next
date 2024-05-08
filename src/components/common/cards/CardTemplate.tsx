1d9b61d97c2fa03b5c2780f2d551b4efd24e86be
SALMAN <elfadili.salman@gmail.com>
2024-05-08 23:36:21 +0100

    fix username position

--- src/components/common/cards/CardTemplate.tsx
+++ src/components/common/cards/CardTemplate.tsx
@@ -155,6 +155,7 @@ function CardTemplate({ template }: CardTemplateProps) {
                   fontSize={13}
                   fontWeight={400}
                   color={alpha(theme.palette.onSurface, 0.75)}
+                  ml={"auto"}
                 >
                   by {template.created_by.first_name || template.created_by.username}
                 </Typography>
