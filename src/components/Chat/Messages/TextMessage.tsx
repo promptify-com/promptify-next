21c6dae1ba6a15525da61a78e65cef602c13e2a4
Saad HARAKAT <saad.harakat5@gmail.com>
2024-05-22 17:41:41 +0100

    Bug/qa bug report 3 (#965)
    * solve save prompt with blank answer.
    
    * Adding padding between chat and response section.
    
    * not allowing to edit the answer once response has generated.
    
    * Solving showing in half window, user has to scroll above to see whole response.

--- src/components/Chat/Messages/TextMessage.tsx
+++ src/components/Chat/Messages/TextMessage.tsx
@@ -18,6 +18,7 @@ import type { IAnswer, IMessage } from "@/components/Prompt/Types/chat";
 interface Props {
   message: IMessage;
   onScrollToBottom: () => void;
+  lastMessage: IMessage;
 }
 
 interface MessageContentProps {
@@ -44,11 +45,12 @@ const MessageContent = memo(({ content, shouldStream, onStreamingFinished }: Mes
   return <>{streamedText}</>;
 });
 
-const Message = ({ message, onScrollToBottom }: Props) => {
+const Message = ({ message, onScrollToBottom, lastMessage }: Props) => {
   const { inputs, answers } = useAppSelector(state => state.chat ?? initialChatState);
   const [isHovered, setIsHovered] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [messageContent, setMessageContent] = useState(message.text);
+  const [originalContent, setOriginalContent] = useState(message.text);
 
   const dispatch = useAppDispatch();
 
@@ -88,13 +90,21 @@ const Message = ({ message, onScrollToBottom }: Props) => {
     dispatch(setAnswers(_answers));
   };
 
-  const handleEditMessage = (value: string) => {
-    setMessageContent(value);
-    dispatchUpdateAnswers(value);
+  const handleSaveClick = () => {
+    const trimmedContent = messageContent.trim();
+
+    if (!trimmedContent) {
+      setMessageContent(originalContent);
+      dispatchUpdateAnswers(originalContent);
+    } else {
+      setOriginalContent(trimmedContent);
+      dispatchUpdateAnswers(trimmedContent);
+    }
+    setIsEditing(false);
   };
 
-  const allowEditMessage = isEditable && fromUser && !isEditing;
-
+  const isExecutionMessage = lastMessage.type === "spark";
+  const allowEditMessage = isEditable && fromUser && !isEditing && !isExecutionMessage;
   if (message.type !== "text") {
     return;
   }
@@ -160,8 +170,8 @@ const Message = ({ message, onScrollToBottom }: Props) => {
               <>
                 <InputBase
                   value={messageContent}
-                  onChange={event => handleEditMessage(event.target.value)}
-                  onBlur={() => setIsEditing(false)}
+                  onChange={event => setMessageContent(event.target.value)}
+                  onBlur={handleSaveClick}
                   autoFocus
                   sx={{
                     color: "onPrimary",
@@ -173,7 +183,7 @@ const Message = ({ message, onScrollToBottom }: Props) => {
                   size="small"
                 />
                 <IconButton
-                  onClick={() => setIsEditing(false)}
+                  onClick={handleSaveClick}
                   size="small"
                   sx={iconButtonStyle}
                 >
