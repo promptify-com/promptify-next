4f99571488a69437e2808e7b73789924572eb374
Saad HARAKAT <saad.harakat5@gmail.com>
2024-05-31 16:06:32 +0100

    Enhance/chat cred placehoder (#993)
    * Credential and chat placeholder.
    
    * removing unnecessary left margin.
    
    * Adding placeholder to ScheduledChat.

--- src/components/GPT/ScheduledChatSteps.tsx
+++ src/components/GPT/ScheduledChatSteps.tsx
@@ -12,6 +12,7 @@ import useCredentials from "@/components/Automation/Hooks/useCredentials";
 import ActivateWorkflowMessage from "./ActivateWorkflowMessage";
 import { FREQUENCY_ITEMS } from "./Constants";
 import FrequencyTimeSelector from "./FrequencyTimeSelector";
+import ChatCredentialsPlaceholder from "./ChatCredentialsPlaceholder";
 
 interface Props {
   workflow: IWorkflow;
@@ -37,57 +38,61 @@ export default function ScheduledChatSteps({ workflow }: Props) {
         p: "48px",
       }}
     >
-      {messages.map(message => (
-        <Box
-          key={message.id}
-          sx={{
-            ...(!message.fromUser && {
-              mr: "56px",
-            }),
-            ...(message.fromUser && {
-              ml: "56px",
-            }),
-            ...(message.noHeader && {
-              mt: "-34px",
-            }),
-          }}
-        >
-          {message.type === "text" && <Message message={message} />}
-          {message.type === "credentials" && (
-            <CredentialsContainer
-              message={message.text}
-              workflow={workflow}
-              isScheduled
-            />
-          )}
-          {message.type === "schedule_frequency" && (
-            <Choices
-              message={message.text}
-              items={FREQUENCY_ITEMS}
-              onSelect={frequency => setScheduleFrequency(frequency as FrequencyType)}
-            />
-          )}
-          {message.type === "schedule_time" && (
-            <FrequencyTimeSelector
-              message={message.text}
-              onSelect={setScheduleTime}
-            />
-          )}
-          {message.type === "schedule_providers" && (
-            <ResponseProvidersContainer
-              message={message.text}
-              workflow={workflow}
-              prepareWorkflow={provider => prepareWorkflow(provider)}
-            />
-          )}
-          {message.type === "schedule_activation" && (
-            <ActivateWorkflowMessage
-              message={message}
-              onActivate={activateWorkflow}
-            />
-          )}
-        </Box>
-      ))}
+      {!!messages.length ? (
+        messages.map(message => (
+          <Box
+            key={message.id}
+            sx={{
+              ...(!message.fromUser && {
+                mr: "56px",
+              }),
+              ...(message.fromUser && {
+                ml: "56px",
+              }),
+              ...(message.noHeader && {
+                mt: "-34px",
+              }),
+            }}
+          >
+            {message.type === "text" && <Message message={message} />}
+            {message.type === "credentials" && (
+              <CredentialsContainer
+                message={message.text}
+                workflow={workflow}
+                isScheduled
+              />
+            )}
+            {message.type === "schedule_frequency" && (
+              <Choices
+                message={message.text}
+                items={FREQUENCY_ITEMS}
+                onSelect={frequency => setScheduleFrequency(frequency as FrequencyType)}
+              />
+            )}
+            {message.type === "schedule_time" && (
+              <FrequencyTimeSelector
+                message={message.text}
+                onSelect={setScheduleTime}
+              />
+            )}
+            {message.type === "schedule_providers" && (
+              <ResponseProvidersContainer
+                message={message.text}
+                workflow={workflow}
+                prepareWorkflow={provider => prepareWorkflow(provider)}
+              />
+            )}
+            {message.type === "schedule_activation" && (
+              <ActivateWorkflowMessage
+                message={message}
+                onActivate={activateWorkflow}
+              />
+            )}
+          </Box>
+        ))
+      ) : (
+        <ChatCredentialsPlaceholder />
+      )}
     </Stack>
   );
 }
