2d08b9eebb8434cff9afac095101ba48bcd171d9
Yassine Belkaid <yassine.belkaid87@gmail.com>
2024-05-31 09:12:11 +0100

    chore: lazy load gtp calendar

--- src/components/GPT/Header.tsx
+++ src/components/GPT/Header.tsx
@@ -9,14 +9,20 @@ import { alpha } from "@mui/material";
 import { theme } from "@/theme";
 import Image from "@/components/design-system/Image";
 import { ElectricBoltIcon } from "@/assets/icons/ElectricBoltIcon";
-import DatePickerCalendar from "../GPTs/DatePickerCalendar";
 import type { IWorkflow } from "@/components/Automation/types";
+import lazy from "next/dynamic";
+import { useAppSelector } from "@/hooks/useStore";
+import { initialState } from "@/core/store/chatSlice";
+
+const LazyDateCPickerCalendar = lazy(() => import("@/components/GPTs/DatePickerCalendar"));
 
 interface Props {
   workflow: IWorkflow;
 }
 
 export default function Header({ workflow }: Props) {
+  const clonedWorkflow = useAppSelector(store => store.chat?.clonedWorkflow ?? initialState.clonedWorkflow);
+
   return (
     <Stack
       direction={"row"}
@@ -134,8 +140,7 @@ export default function Header({ workflow }: Props) {
           </Stack>
         </Stack>
       </Stack>
-      {/* TODO: to render only if the template workflow is a scheduling */}
-      <DatePickerCalendar />
+      {workflow.is_schedulable && clonedWorkflow?.periodic_task?.task && <LazyDateCPickerCalendar />}
     </Stack>
   );
 }
