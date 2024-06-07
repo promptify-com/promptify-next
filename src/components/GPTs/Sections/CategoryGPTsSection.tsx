9aebe7222b745573feb6f59402ab6f39845cdb28
nannuflay <mohameddcharafi@gmail.com>
2024-06-07 15:26:51 +0100

    reduce duplicate code

--- src/components/GPTs/Sections/CategoryGPTsSection.tsx
+++ src/components/GPTs/Sections/CategoryGPTsSection.tsx
@@ -1,9 +1,5 @@
-import { Fragment } from "react";
-import Stack from "@mui/material/Stack";
-
-import CarouselSection from "@/components/GPTs/Sections/CarouselSection";
-import WorkflowCard from "@/components/GPTs/WorkflowCard";
 import WorkflowCardPlaceholder from "@/components/GPTs/WorkflowCardPlaceholder";
+import GPTsSection from "@/components/GPTs/Sections/GPTsSection";
 import type { IWorkflowCategory } from "@/components/Automation/types";
 
 interface Props {
@@ -18,22 +14,13 @@ function CategoryGPTsSection({ workflowCategories, isLoading }: Props) {
   return (
     <>
       {workflowCategories?.map((workflows, index) => (
-        <Fragment key={`${workflows.category}-${index}`}>
-          <CarouselSection
-            header={workflows.category || ""}
-            subheader="Enhance your daily routines and professional output with AI-driven solutions. 
-            Achieve more with less effort, maximizing your efficiency and effectiveness."
-          >
-            {workflows.templates.map(workflow => (
-              <Stack key={workflow.id}>
-                <WorkflowCard
-                  templateWorkflow={workflow}
-                  lastExecuted={null}
-                />
-              </Stack>
-            ))}
-          </CarouselSection>
-        </Fragment>
+        <GPTsSection
+          key={`${workflows.category}-${index}`}
+          workflows={workflows.templates}
+          isLoading={isLoading}
+          header={workflows.category || ""}
+          subheader="Enhance your daily routines and professional output with AI-driven solutions. "
+        />
       ))}
     </>
   );
