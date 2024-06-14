b0accafa8fb9cccabd930463be91afd0f30298b7
SALMAN <elfadili.salman@gmail.com>
2024-06-14 16:29:03 +0100

    implement auto scrolling for steps/streaming/non-streaming messages (#1094)

--- src/hooks/useScrollToElement.ts
+++ src/hooks/useScrollToElement.ts
@@ -1,19 +1,12 @@
-import { useEffect, useState } from "react";
-
 export const useScrollToElement = (behavior: "auto" | "smooth" = "auto") => {
-  const [querySelector, setQuerySelector] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (querySelector) {
-      const element = document.querySelector(querySelector);
-      if (element) {
-        const y =
-          element.getBoundingClientRect().top + window.scrollY - window.innerHeight / 2 + element.clientHeight / 2;
-        window.scrollTo({ top: y, behavior });
-        setQuerySelector(null);
-      }
+  const scrollTo = (querySelector: string) => {
+    const element = document.querySelector(querySelector);
+    if (element) {
+      const y =
+        element.getBoundingClientRect().top + window.scrollY - window.innerHeight / 2 + element.clientHeight / 2;
+      window.scrollTo({ top: y, behavior });
     }
-  }, [querySelector]);
+  };
 
-  return setQuerySelector;
+  return scrollTo;
 };
