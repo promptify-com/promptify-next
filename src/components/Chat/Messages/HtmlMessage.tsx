effd3cf47a903d0748cb0f167c0dcfabcc64e39f
SALMAN <elfadili.salman@gmail.com>
2024-08-01 02:20:46 +0100

    cleanup MessageContentWithHTML

--- src/components/Chat/Messages/HtmlMessage.tsx
+++ src/components/Chat/Messages/HtmlMessage.tsx
@@ -37,7 +37,7 @@ const MessageContentWithHTML = memo(({ content, shouldStream, onStreamingFinishe
       dispatch(setIsSimulationStreaming(false));
       onStreamingFinished();
     }
-  }, [streamedText, hasFinished, dispatch, onStreamingFinished]);
+  }, [streamedText, hasFinished]);
 
   return (
     <div
