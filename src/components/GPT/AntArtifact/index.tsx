e42452d3ddbfbe45ff74d3e18e34c3a35699fb8f
Belaidi Mohammed <46646596+mbelaidi@users.noreply.github.com>
2024-10-03 17:22:35 +0100

    Feature/prfe 39 make chat message render a reactjs code snippet (#1401)
    * test: testing rendering reactjs Snippet
    
    * test: add libraries
    
    * test: fast render content
    
    * feat: integrate components in live chat
    
    * feat: adding tabs switch
    
    * feat: styling Tabs
    
    * fix: reuse ExecutionContent components
    
    * feat: adding title and copy button
    
    * fix: comment cleaning
    
    * improvement : lazy load components
    
    * fix : fix the width issue
    
    * fix : alignment issue
    
    * fix : parsing issues
    
    * fix : use mui components

---
+++ src/components/GPT/AntArtifact/index.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import * as Materiel from "@mui/material";
+import Card from "@mui/material/Card";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import * as Recharts from "recharts";
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live-runner";
+import AntArtifactTabs from "@/components/GPT/AntArtifact/tabs";
+import CopyButton from "./copyButton";
+
+interface Props {
+  content: string;
+  title?: string;
+}
+
+const scope = { import: { react: React, recharts: Recharts, "@mui/material": Materiel } };
+
+const AntArtifactComponent = ({ content, title }: Props) => {
+  const startIndex = content.indexOf(">") + 1;
+  const code = content.substring(startIndex).trim();
+  const [tab, setTab] = React.useState("preview");
+
+  return (
+    <Card
+      elevation={0}
+      sx={{ borderRadius: 4, marginY: 2, pY: 2 }}
+    >
+      <Stack
+        direction="row"
+        sx={{ p: 1, alignItems: "center", justifyContent: "space-between" }}
+      >
+        <Typography
+          variant="subtitle2"
+          fontWeight={600}
+        >
+          {title}
+        </Typography>
+        <AntArtifactTabs
+          setTab={setTab}
+          tab={tab}
+        />
+      </Stack>
+      <LiveProvider
+        code={code}
+        scope={scope}
+        language={"javascript"}
+      >
+        {tab === "code" && (
+          <LiveEditor
+            style={{ margin: "auto" }}
+            disabled
+          />
+        )}
+        {tab === "preview" && (
+          <Stack sx={{ p: 2 }}>
+            <LivePreview Component={"div"} />
+          </Stack>
+        )}
+        <Stack
+          direction="row"
+          sx={{ p: 1, alignItems: "center", justifyContent: "space-between" }}
+        >
+          <LiveError />
+          <Box sx={{ flexGrow: 1 }} />
+          <CopyButton code={code} />
+        </Stack>
+      </LiveProvider>
+    </Card>
+  );
+};
+
+export default AntArtifactComponent;
